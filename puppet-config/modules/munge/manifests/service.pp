d1b8f6851bce96931b99d784c9dbd5d057e54ee3
Antonio J. Russo <antonio.russo@unc.edu.ar>
2016-03-11 15:00:52 +0100

    Wrong variable names

--- puppet-config/modules/munge/manifests/service.pp
+++ puppet-config/modules/munge/manifests/service.pp
@@ -1,15 +1,15 @@
 class munge::service {
 
-  if $service::service_manage == true {
+  if $munge::service_manage == true {
 
     if ! ($munge::service_ensure in [ 'running', 'stopped' ]) {
       fail('service_ensure parameter must be running or stopped')
     }
 
     service { $munge::service_name :
-      ensure     => $ntp::service_ensure,
-      enable     => $ntp::service_enable,
-      name       => $ntp::service_name,
+      ensure     => $munge::service_ensure,
+      enable     => $munge::service_enable,
+      name       => $munge::service_name,
     }
   }
 }
