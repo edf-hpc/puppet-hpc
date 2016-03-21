6295c25aaed55489b296c43a767cb8a8ac4d8573
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-03-21 09:11:29 +0000

    Remove dependency between slurmctld::params and slurmcommons

--- puppet-config/modules/slurmctld/manifests/params.pp
+++ puppet-config/modules/slurmctld/manifests/params.pp
@@ -1,5 +1,4 @@
 class slurmctld::params {
-
   $enable_lua        = true
   $enable_topology   = true
   $enable_wckeys     = false
@@ -13,27 +12,31 @@ class slurmctld::params {
 
   ### Configuration ###
   $config_manage     = true
-  $topo_conf_file    = "${slurmcommons::conf_dir_path}/topology.conf"
-  $submit_lua_script = "${slurmcommons::conf_dir_path}/job_submit.lua"
+  $conf_dir_path     = '/etc/slurm-llnl'
+  $topo_conf_file    = "${conf_dir_path}/topology.conf"
+  $submit_lua_script = "${conf_dir_path}/job_submit.lua"
 
   $topology_conf     = [
     'SwitchName=switch1 Nodes=localhost',
   ]
 
-
-
   ### Package & Configuration ###
   $package_ensure    = 'present'
+  $submit_lua_source = 'puppet:///modules/slurmctld/job_submit.lua'
   case $::osfamily {
     'RedHat': {
       $package_manage    =  true
-      $package_name      = ['slurm', 'slurm-devel']
-      $submit_lua_source = 'puppet:///modules/slurmctld/job_submit.lua'
+      $package_name      = [
+        'slurm', 
+        'slurm-devel',
+      ]
     }
     'Debian': {
       $package_manage    =  true
-      $package_name      = ['slurmctld','slurm-llnl-job-submit-plugin']
-      $submit_lua_source = "${slurmcommons::bin_dir_path}/job_submit.lua"
+      $package_name      = [
+        'slurmctld',
+        'slurm-llnl-job-submit-plugin',
+      ]
     }
     default: {
       $package_manage  =  false
