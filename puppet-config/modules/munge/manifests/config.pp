9b07d528029f4b444872ede7bd6bd63cfe045beb
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-04-08 13:47:10 +0100

    Bugfix

--- puppet-config/modules/munge/manifests/config.pp
+++ puppet-config/modules/munge/manifests/config.pp
@@ -7,6 +7,6 @@ class munge::config {
   file { $munge::auth_key_name :
     content => decrypt($munge::auth_key_source, $munge::decrypt_passwd),
     mode    => $munge::auth_key_mode,
-    require => $auth_key_path,
+    require => File[$munge::auth_key_path],
   }
 }
