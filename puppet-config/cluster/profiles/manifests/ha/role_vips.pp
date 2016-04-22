28b7c67c979ad6d402926e6d9900ad6d663b6015
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-04-22 13:37:17 +0100

    Add HA Virtual IP addresses for role profile

---
+++ puppet-config/cluster/profiles/manifests/ha/role_vips.pp
@@ -0,0 +1,27 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# This profile defines Virtual IP addresses associated with the role as a
+# whole and not specific to a service.
+# Virtual IP Addresses should be defined in hiera by following the hpc_ha
+# module vip resource syntax.
+
+class profiles::ha::role_vips {
+  include ::hpc_ha
+
+  $vips = hiera_hash("profiles::ha::role_vips", {})
+  create_resources(hpc_ha::vip, $vips)
+}
+
