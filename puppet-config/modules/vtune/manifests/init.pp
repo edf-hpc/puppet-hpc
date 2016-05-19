b8503999497eafd8e709b0ca9588a7836fb9b29e
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-05-19 17:12:47 +0100

    Add vtune module

---
+++ puppet-config/modules/vtune/manifests/init.pp
@@ -0,0 +1,34 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class vtune (
+  $packages         = $vtune::params::packages,
+  $packages_ensure  = $vtune::params::packages_ensure,
+  $default_file     = $vtune::params::default_file,
+  $default_options  = $vtune::params::default_options,
+) inherits vtune::params {
+
+  validate_array($packages)
+  validate_string($packages_ensure)
+  validate_absolute_path($default_file)
+  validate_hash($default_options)
+
+  anchor { 'vtune::begin': } ->
+  class { '::vtune::install': } ->
+  class { '::vtune::config': } ->
+  class { '::vtune::service': } ->
+  anchor { 'vtune::end': }
+
+}
---
+++ puppet-config/modules/vtune/manifests/params.pp
@@ -0,0 +1,31 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class vtune::params {
+
+#### Module variables
+
+  $packages_ensure = 'latest'
+  $packages        = ['sep-modules','sep-modules-scripts']
+  $default_file    = '/etc/default/sep-scripts'
+
+#### Defaults values
+  $default_options = {
+    'DRIVER_DIRECTORY' => '/lib/modules/3.16.0-4-amd64/extra',
+    'DRIVER_GROUP'     => 'vtune-users',
+    'DRIVER_PERMS'     => '660',
+  }
+
+}
