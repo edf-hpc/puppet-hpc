f90366357499c2145d1a0c774cdb0de451b55718
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-05-19 10:54:40 +0100

    Add cpufreq module

---
+++ puppet-config/modules/cpufreq/manifests/init.pp
@@ -0,0 +1,34 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class cpufreq (
+  $packages         = $cpufreq::params::packages,
+  $packages_ensure  = $cpufreq::params::packages_ensure,
+  $default_file     = $cpufreq::params::default_file,
+  $default_options  = $cpufreq::params::default_options,
+) inherits cpufreq::params {
+
+  validate_array($packages)
+  validate_string($packages_ensure)
+  validate_absolute_path($default_file)
+  validate_hash($default_options)
+
+  anchor { 'cpufreq::begin': } ->
+  class { '::cpufreq::install': } ->
+  class { '::cpufreq::config': } ->
+  class { '::cpufreq::service': } ->
+  anchor { 'cpufreq::end': }
+
+}
