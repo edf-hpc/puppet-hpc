90663f0f905657c7d74280967ec4bb94f28ee4fc
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-05-20 13:58:24 +0100

    Add codes module

---
+++ puppet-config/modules/codes/manifests/init.pp
@@ -0,0 +1,28 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class codes (
+  $packages_ensure  = $codes::params::packages_ensure,
+  $packages,
+) inherits codes::params {
+
+  validate_array($packages)
+  validate_string($packages_ensure)
+
+  anchor { 'codes::begin': } ->
+  class { '::codes::install': } ->
+  anchor { 'codes::end': }
+
+}
