522809a8eb5aae592241962699ad8c9e265bb771
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-05-23 17:31:14 +0100

    mod:hpclib Replace inline_template by hpclib

--- puppet-config/modules/hpclib/manifests/systemd_service.pp
+++ puppet-config/modules/hpclib/manifests/systemd_service.pp
@@ -1,7 +1,7 @@
 ##########################################################################
 #  Puppet configuration file                                             #
 #                                                                        #
-#  Copyright (C) 2014-2015 EDF S.A.                                      #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
 #  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
 #                                                                        #
 #  This program is free software; you can redistribute in and/or         #
@@ -13,19 +13,24 @@
 #  GNU General Public License for more details.                          #
 ##########################################################################
 
-define hpclib::systemd_service($target,$config) {
+define hpclib::systemd_service(
+  $target,
+  $config,
+) {
 
-  $service_name  = inline_template('<%= File.basename(@target) %>')
+  $service_name = basename($target)
 
   file { $target :
-    content    => template('hpclib/systemd_service.erb'),
+    content => template('hpclib/systemd_service.erb'),
   }
 
   ### Systemctl execution on supported environments ###
 
   exec { $target :
-    command    => "/bin/systemctl daemon-reload && /bin/systemctl enable ${service_name}",
-    require    => File[$target],
+    command     => "/bin/systemctl daemon-reload && /bin/systemctl enable ${service_name}",
+    refreshonly => true,
+    subscribe   => File[$target],
+    require     => File[$target],
   }
 
 }
