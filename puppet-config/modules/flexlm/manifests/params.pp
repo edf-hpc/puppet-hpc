3f17761d9e8f57bcc0d49d6d9e323c9258772b9f
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-05-24 14:02:47 +0100

    Add flexlm module

---
+++ puppet-config/modules/flexlm/manifests/params.pp
@@ -0,0 +1,45 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class flexlm::params {
+
+#### Module variables
+
+  $service_ensure = 'running'
+  $service_enable = true 
+
+#### Defaults values
+  $binary_path     = '/usr/local/bin/lmgrd'
+  $vendor_name     = ''
+  $user            = 'flexlm'
+  $user_home       = "/home/${user}"
+  $logfile         = "/var/log/flexlm_${vendor_name}"
+  $systemd_service = 'flexlm'
+  $systemd_config  = {
+    'Unit'    => {
+      'Description'         => "FlexLM server for ${vendor_name} products",
+      'Documentation'       => "file:/opt/${vendor_name}/flexlm/README",
+      'After'               => 'remote-fs.target network.target home.mount',
+    },
+    'Service' => {
+      'Type'                => 'forking',
+      'ExecStart'           => "${binary_path} -c ${license_path} -l ${logfile}",
+      'User'                => $user,
+    },
+    'Install' => {
+      'WantedBy'            => 'multi-user.target',
+    },
+  }
+}
---
+++ puppet-config/modules/flexlm/manifests/service.pp
@@ -0,0 +1,65 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+define flexlm::service (
+  $license_path,
+  $service_ensure  = $flexlm::params::service_ensure,
+  $service_enable  = $flexlm::params::service_enable,
+  $binary_path     = $flexlm::params::binary_path,
+  $vendor_name     = $flexlm::params::vendor_name,
+  $user            = $flexlm::params::user,
+  $user_home       = $flexlm::params::user_home,
+  $logfile         = $flexlm::params::logfile,
+  $systemd_service = $flexlm::params::systemd_service,
+  $systemd_config  = $flexlm::params::systemd_config,
+) {
+
+  validate_absolute_path($license_path)
+  validate_string($service_ensure)
+  validate_bool($service_enable)
+  validate_absolute_path($binary_path)
+  validate_string($vendor_name)
+  validate_string($user)
+  validate_absolute_path($user_home)
+  validate_absolute_path($logfile)
+  validate_string($systemd_service)
+  validate_hash($systemd_config)
+
+  hpclib::systemd_service { $systemd_service :
+    target  => $systemd_service,
+    config  => $systemd_config,
+    require => [User[$user],File[$logfile]]
+  }
+  
+  user { $user :
+    ensure => present,
+    home   => $user_home,
+    system => true,
+  }
+
+  file { $logfile :
+    ensure => file,
+    owner  => $user,
+  }
+
+  service { "flexlm_${vendor_name}" :
+    ensure     => $service_ensure,
+    enable     => $service_enable,
+    require    => [
+      Hpclib::Systemd_service[$systemd_service],
+      File[$logfile],
+    ],
+  }
+}
