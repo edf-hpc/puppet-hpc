5589a72cb7f8aa8b17327cab32595efd9763a080
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-05-25 16:51:09 +0100

    prof:metrics New profiles to collect resources and relay them
    Rekay profile is configured through hiera.

---
+++ puppet-config/cluster/profiles/manifests/metrics/collect_base.pp
@@ -0,0 +1,77 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class profiles::metrics::collect_base {
+  include ::collectd
+
+  # The default relay is <prefix><role> of the role having the 
+  # metrics::relay role
+  $cluster_prefix = hiera('cluster_prefix')
+  $relay_host_default = "${cluster_prefix}${my_metrics_relay}"
+  $relay_host = hiera( 'profiles::metrics::relay_host', $relay_host_default)
+
+  ::collectd::plugin::write_graphite::carbon { 'relay':
+    graphitehost      => $relay_host,
+    graphiteport      => 2003,
+    graphiteprefix    => '',
+    protocol          => 'tcp',
+    alwaysappendds    => false,
+    storerates        => true,
+    separateinstances => false,
+    logsenderrors     => true
+  }
+
+  # Metrics we want pretty much everywhere
+  include ::collectd::plugin::cpu
+  include ::collectd::plugin::load
+  include ::collectd::plugin::uptime
+  include ::collectd::plugin::memory
+
+  ::collectd::plugin { 'match_regex':
+    ensure => present,
+  }
+
+  ::collectd::plugin::aggregation::aggregator { 'cpu':
+    plugin           => 'cpu',
+    type             => 'cpu',
+    groupby          => ["Host", "TypeInstance",],
+    calculateaverage => true,
+    calculatesum     => true,
+  }
+    
+  class { '::collectd::plugin::chain':
+    chainname     => "PostCache",
+    defaulttarget => "write",
+    rules         => [
+      {
+        'match' => {
+          'type'    => 'regex',
+          'matches' => {
+            'Plugin'         => '^cpu$',
+            'PluginInstance' => '^[0-9]+$',
+          }
+        },
+        'targets' => [
+          {
+            'type'      => 'write',
+            'attributes' => {
+              'Plugin' => 'aggregation',
+            },
+          },
+        ],
+      },
+    ],
+  }
+}
---
+++ puppet-config/cluster/profiles/manifests/metrics/relay.pp
@@ -0,0 +1,28 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class profiles::metrics::relay {
+  include ::carboncrelay
+
+  $clusters = hiera_hash('profiles::metrics::relay_destination_clusters')
+  create_resources(::carboncrelay::cluster, $clusters)
+
+  $match_rules = hiera_hash('profiles::metrics::relay_match_rules')
+  create_resources(::carboncrelay::match, $match_rules)
+
+  $rewrite_rules = hiera_hash('profiles::metrics::relay_rewrite_rules')
+  create_resources(::carboncrelay::rewrite, $rewrite_rules)
+  
+}
