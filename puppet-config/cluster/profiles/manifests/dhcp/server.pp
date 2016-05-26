64d6852a68e6845fdae8277af23a90b37c265462
Cécile YOSHIKAWA <cecile.yoshikawa@edf.fr>
2016-05-26 14:00:07 +0100

    prof:dhcp Retrieve failover role depending on hostname index

--- puppet-config/cluster/profiles/manifests/dhcp/server.pp
+++ puppet-config/cluster/profiles/manifests/dhcp/server.pp
@@ -14,11 +14,22 @@ class profiles::dhcp::server {
   $dhcp_config     = $::dhcpconfig
   $prefix          = hiera('cluster_prefix')
   
+  # Primary server has an index of 1
+  # Secondary server has an index of 2 
   if $::puppet_index == '1' {
     $peer_address  = $::hostfile["${prefix}${::puppet_role}2"]
+    $failover_add = {
+      role            => 'primary',
+      load_split      => '128',
+    }
   } else {
     $peer_address  = $::hostfile["${prefix}${::puppet_role}1"]
+    $failover_add = {
+      role            => 'secondary',
+      load_split      => '',
+    }
   }
+  $_failover = merge($failover, $failover_add)
   $virtual_address = $::hostfile["${prefix}${::puppet_role}"]
 
   # Pass config options as a class parameter
@@ -31,7 +42,7 @@ class profiles::dhcp::server {
     ipxebin         => $ipxebin,
     default_options => $default_options,
     global_options  => $global_options,
-    failover        => $failover,
+    failover        => $_failover,
     sharednet       => $sharednet,
     includes        => $includes,
   }
