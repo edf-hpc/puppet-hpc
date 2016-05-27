6eae86093cd65d2b97cb5c2ebf588a39182d7bc7
Antonio J. Russo <antonio.russo@unc.edu.ar>
2016-05-27 13:47:04 -0300

    New module cpupower (replace cpufreq in RHEL systems)

---
+++ puppet-config/modules/cpupower/manifests/init.pp
@@ -0,0 +1,34 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class cpupower (
+  $packages         = $cpupower::params::packages,
+  $packages_ensure  = $cpupower::params::packages_ensure,
+  $default_file     = $cpupower::params::default_file,
+  $default_options  = $cpupower::params::default_options,
+) inherits cpupower::params {
+
+  validate_array($packages)
+  validate_string($packages_ensure)
+  validate_absolute_path($default_file)
+  validate_hash($default_options)
+
+  anchor { 'cpupower::begin': } ->
+  class { '::cpupower::install': } ->
+  class { '::cpupower::config': } ->
+  class { '::cpupower::service': } ->
+  anchor { 'cpupower::end': }
+
+}
---
+++ puppet-config/modules/cpupower/manifests/params.pp
@@ -0,0 +1,46 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class cpupower::params {
+
+#### Module variables
+
+  $packages_ensure = 'latest'
+
+  case $::osfamily {
+    'RedHat': {
+      $config_manage   = true
+      $service_manage  = true
+      $packages        = ['kernel-tools']
+      $default_file    = '/etc/sysconfig/cpupower'
+      $service         = 'cpupower'
+      $service_ensure  = running
+      $service_enable  = true
+    }
+    'Debian': {
+      $config_manage   = false
+      $service_manage  = false
+      $packages        = ['linux-cpupower']
+    }
+  }
+
+
+#### Defaults values
+  $default_options = {
+    'CPUPOWER_START_OPTS' => '"frequency-set -g performance"'
+    'CPUPOWER_STOP_OPTS'  => '"frequency-set -g ondemand"'
+  }
+
+}
