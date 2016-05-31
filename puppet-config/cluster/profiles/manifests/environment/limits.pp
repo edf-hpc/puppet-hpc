d726ac35d95da802873be8614355c854714140ea
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-05-31 16:14:43 +0100

    prof:environment Add a profile for resources limits in PAM

---
+++ puppet-config/cluster/profiles/manifests/environment/limits.pp
@@ -0,0 +1,29 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class profiles::environment::limits {
+  $ruleset = hiera('profiles::environment::limits_ruleset', undef)
+
+
+  if $ruleset {
+    $rules = hiera_hash("profiles::environment::limits_${ruleset}")
+    class { '::pam::limits':
+      rules => $rules,
+    }
+  } else {
+    # Just setup limits, rules are coming from autolookup or elswhere
+    include ::pam::limits
+  }
+}
