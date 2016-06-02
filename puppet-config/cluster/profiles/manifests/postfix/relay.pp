6d1c7bcc858978c392bd8ad080032e21f3b1047e
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-02 13:18:47 +0100

    prof:postfix Refactor with new variables names

--- puppet-config/cluster/profiles/manifests/postfix/relay.pp
+++ puppet-config/cluster/profiles/manifests/postfix/relay.pp
@@ -1,19 +1,31 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class profiles::postfix::relay {
 
   ## Hiera lookups
-
-  $pos_opts     = hiera_hash('profiles::postfix::relay::pos_opts')
+  $options      = hiera_hash('profiles::postfix::relay::config_options')
   $net_topology = hiera_hash('net_topology')
   $network      = "${net_topology['clusterloc']['ipnetwork']}${net_topology['clusterloc']['cidr']}"
-  $net_opts     = {
-    mynetworks    => "$network 127.0.0.0/8",
+  $net_options     = {
+    mynetworks => "$network 127.0.0.0/8",
   }
-
-  $cfg_opts = merge($pos_opts,$net_opts)
-
+  $full_options = merge($options,$net_options)
 
   # Pass config options as a class parameter
   class { '::postfix':
-    cfg_opts    => $cfg_opts,
+    config_options => $full_options,
   }
 }
