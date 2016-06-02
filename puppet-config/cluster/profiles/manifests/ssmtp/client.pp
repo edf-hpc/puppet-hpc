b9c621f00286a4d7ac42c9ab1bbee1e7f39d2fcb
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-02 13:13:26 +0100

    prof:ssmtp Use new variables names for ssmtp

--- puppet-config/cluster/profiles/manifests/ssmtp/client.pp
+++ puppet-config/cluster/profiles/manifests/ssmtp/client.pp
@@ -1,11 +1,24 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class profiles::ssmtp::client {
-
   ## Hiera lookups
-
-  $ext_cfg_opts      = hiera_hash('profiles::ssmtp::client::ssm_opts')
+  $config_options  = hiera_hash('profiles::ssmtp::client::config_options')
 
   # Pass config options as a class parameter
   class { '::ssmtp':
-    ext_cfg_opts       => $ext_cfg_opts,
+    config_options => $config_options,
   }
 }
