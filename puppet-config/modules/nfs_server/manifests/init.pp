0062e79b6e9806edd430ba96be20c5aab2243066
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-03 17:03:46 +0100

    mod:nfs_server Style cleanup

--- puppet-config/modules/nfs_server/manifests/init.pp
+++ puppet-config/modules/nfs_server/manifests/init.pp
@@ -1,19 +1,35 @@
-#
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class nfs_server (
-  $cfg                = $nfs_server::params::cfg,
-  $pkgs               = $nfs_server::params::pkgs,
-  $pkgs_ensure        = $nfs_server::params::pkgs_ensure, 
-  $serv               = $nfs_server::params::serv,
-  $serv_ensure        = $nfs_server::params::serv_ensure,
+  $exports_file    = $::nfs_server::params::exports_file,
+  $packages        = $::nfs_server::params::packages,
+  $packages_ensure = $::nfs_server::params::packages_ensure, 
+  $service         = $::nfs_server::params::service,
+  $service_ensure  = $::nfs_server::params::service_ensure,
 ) inherits nfs_server::params {
 
-  validate_absolute_path($cfg)
-  validate_array($pkgs)
-  validate_string($pkgs_ensure)
-  validate_string($serv)
-  validate_string($serv_ensure)
+  validate_absolute_path($exports_file)
+  validate_array($packages)
+  validate_string($packages_ensure)
+  validate_string($service)
+  validate_string($service_ensure)
 
-  class { 'nfs_server::install': }
-  class { 'nfs_server::service': }
+  anchor { 'nfs_server::begin': } ->
+  class { 'nfs_server::install': } ->
+  class { 'nfs_server::service': } ->
+  anchor { 'nfs_server::end': }
 
 }
--- puppet-config/modules/nfs_server/manifests/service.pp
+++ puppet-config/modules/nfs_server/manifests/service.pp
@@ -1,9 +1,22 @@
-#
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class nfs_server::service inherits nfs_server {
 
-  service { $nfs_server::serv :
-    ensure       => $nfs_server::serv_ensure,
-    subscribe    => File[$nfs_server::cfg],
+  service { $service:
+    ensure => $service_ensure,
   }
 
 }
