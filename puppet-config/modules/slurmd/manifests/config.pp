3357813c1a00f6c7ed7f491366ff1a5e7c635466
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-06-03 13:30:09 +0100

    Change ressource name as it used to create /etc/rsyslog.d/ file

--- puppet-config/modules/slurmd/manifests/config.pp
+++ puppet-config/modules/slurmd/manifests/config.pp
@@ -34,7 +34,7 @@ class slurmd::config {
       create_resources(file,$cgroup_links,$cgroup_ln_default)
     }
   }
-    rsyslog::imfile { '/var/log/slurm-llnl/slurmd.log':
+    rsyslog::imfile { 'slurmd':
       file_name     => '/var/log/slurm-llnl/slurmd.log',
       file_tag      => 'slurmd:',
       file_facility => 'info',
