45244a45839ca0b3172baebe513164d01406a3c7
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-06-03 10:18:16 +0100

    Add logrotate to logsserver profile

--- puppet-config/cluster/profiles/manifests/log/server.pp
+++ puppet-config/cluster/profiles/manifests/log/server.pp
@@ -4,11 +4,27 @@ class profiles::log::server {
 
   $server_dir    = hiera('profiles::log::server::server_dir')
   $custom_config = hiera('profiles::log::server::custom_config')
-
+  $firstaction   = hiera('profiles::log::server::logrotate_firstaction')
 
   # Pass config options as a class parameter
   class { '::rsyslog::server':
     server_dir    => $server_dir,
     custom_config => $custom_config,
   }
+  logrotate::rule { 'remotelog':
+    path          => "${server_dir}*/*",
+    compress      => true,
+    missingok     => true,
+    firstaction   => $firstaction,
+    copytruncate  => true,
+    create        => false,
+    delaycompress => true,
+    mail          => false,
+    rotate        => '30',
+    sharedscripts => true,
+    size          => '5M',
+    rotate_every  => day,
+    postrotate    => 'invoke-rc.d rsyslog rotate > /dev/null',
+  }
+
 }
