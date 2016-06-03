ddba916d55930982d37444c6730afaf199a899ad
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-03 17:04:57 +0100

    prof:nfs Update to match style cleanup (add profiles:: to hiera)

--- puppet-config/cluster/profiles/manifests/nfs/mounts.pp
+++ puppet-config/cluster/profiles/manifests/nfs/mounts.pp
@@ -1,11 +1,25 @@
-#
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class profiles::nfs::mounts {
 
   # Hiera lookups
-  $to_mount = hiera_hash('nfs::to_mount')
+  $to_mount = hiera_hash('profiles::nfs::to_mount')
 
   # Initialize nfs_client
-  class { '::nfs_client': }
+  include ::nfs_client
  
   # Mount all the specified directories
   create_resources('::nfs_client::mount', $to_mount)
