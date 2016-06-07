f5933b2dce9dc444fe199008a6d46255f8442625
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-06-07 14:45:01 +0100

    lint : blanc spaces, misdeclared variable

--- puppet-config/modules/boottftp/manifests/init.pp
+++ puppet-config/modules/boottftp/manifests/init.pp
@@ -10,7 +10,7 @@ class boottftp (
   validate_absolute_path($config_dir_ftp)
   validate_absolute_path($ipxe_efi_source)
   validate_absolute_path($ipxe_legacy_source)
-  
+
   $ipxe_efi_file                   = "${config_dir_ftp}/ipxe.efi"
   $ipxe_legacy_file                = "${config_dir_ftp}/ipxe.legacy"
 
