018cf080d9708de1edc8cfabc3bb9fef23321885
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-23 17:07:28 +0100

    mod:slurmd Lint fixes

--- puppet-config/modules/slurmd/manifests/config.pp
+++ puppet-config/modules/slurmd/manifests/config.pp
@@ -1,3 +1,18 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class slurmd::config {
 
   if $slurmd::config_manage {
@@ -11,14 +26,14 @@ class slurmd::config {
         notify => Class['::slurmd::service'],
       }
 
-      ensure_resource('file', "${slurmd::cgroup_rel_path}",{'ensure' => 'directory' })
+      ensure_resource('file', $slurmd::cgroup_rel_path, {'ensure' => 'directory' })
 
       file { $slurmd::cgroup_relscript_file:
-        mode       => 0744, # must be executable only by root
-        owner      => 'root',
-        source     => $slurmd::cgroup_relscript_src,
-        require    => File[$slurmd::cgroup_rel_path],
-      }  
+        mode    => '0744', # must be executable only by root
+        owner   => 'root',
+        source  => $slurmd::cgroup_relscript_src,
+        require => File[$slurmd::cgroup_rel_path],
+      }
 
       $cgroup_links = {
         "${slurmd::cgroup_rscpuset_file}" => { },
@@ -27,9 +42,9 @@ class slurmd::config {
       }
 
       $cgroup_ln_default = {
-        ensure     => link,
-        target     => $slurmd::cgroup_relscript_file,
-        require    => File[$slurmd::cgroup_relscript_file],
+        ensure  => link,
+        target  => $slurmd::cgroup_relscript_file,
+        require => File[$slurmd::cgroup_relscript_file],
       }
       create_resources(file,$cgroup_links,$cgroup_ln_default)
     }
