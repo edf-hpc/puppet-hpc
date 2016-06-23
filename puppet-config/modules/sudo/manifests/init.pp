9562062098be1de9241f4cda6c2d5301f7ace7e6
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-23 16:58:53 +0100

    mod:sudo Lint fixes

--- puppet-config/modules/sudo/manifests/init.pp
+++ puppet-config/modules/sudo/manifests/init.pp
@@ -1,3 +1,18 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 #
 class sudo (
   $packages               = $sudo::params::packages,
@@ -5,11 +20,11 @@ class sudo (
   $config_file            = $sudo::params::config_file,
   $config_options         = $sudo::params::config_options,
 ) inherits sudo::params {
-  
+
   validate_array($packages)
   validate_string($packages_ensure)
   validate_absolute_path($config_file)
-  validate_array($config_options) 
+  validate_array($config_options)
 
   anchor { 'sudo::begin': } ->
   class { '::sudo::install': } ->
