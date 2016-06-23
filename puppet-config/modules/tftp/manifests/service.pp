5d1539bad5acb1a11f2847813c737ac61da6cff1
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-23 16:57:48 +0100

    mod:tftp Lint fixes

--- puppet-config/modules/tftp/manifests/service.pp
+++ puppet-config/modules/tftp/manifests/service.pp
@@ -1,10 +1,21 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class tftp::service inherits tftp {
 
-  if ! ($tftp::service_ensure in [ 'running', 'stopped' ]) {
-    fail('service_ensure parameter must be running or stopped')
-  }
-
-  service { $tftp::service :
+  service { $::tftp::service :
     ensure     => $tftp::service_ensure,
   }
 
