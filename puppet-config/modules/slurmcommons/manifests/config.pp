87bbf2b77839297fbf4ef3d7478a1886b56aa800
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-23 17:10:20 +0100

    mod:slurmcommons Lint fixes

--- puppet-config/modules/slurmcommons/manifests/config.pp
+++ puppet-config/modules/slurmcommons/manifests/config.pp
@@ -1,21 +1,36 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class slurmcommons::config {
 
-  ensure_resource('file', "${slurmcommons::bin_dir_path}",{'ensure' => 'directory' })
-  ensure_resource('file', "${slurmcommons::conf_dir_path}",{'ensure' => 'directory' })
-  ensure_resource('file', "${slurmcommons::logs_dir_path}",{'ensure' => 'directory' })
-  ensure_resource('file', "${slurmcommons::script_dir_path}",{'ensure' => 'directory' })
+  ensure_resource('file', $slurmcommons::bin_dir_path,    {'ensure' => 'directory' })
+  ensure_resource('file', $slurmcommons::conf_dir_path,   {'ensure' => 'directory' })
+  ensure_resource('file', $slurmcommons::logs_dir_path,   {'ensure' => 'directory' })
+  ensure_resource('file', $slurmcommons::script_dir_path, {'ensure' => 'directory' })
 
   hpclib::print_config { $slurmcommons::main_conf_file :
-    style        => 'keyval',
-    data         => $slurmcommons::slurm_conf_options,
-    exceptions   => ['Include'],
-    require      => File["$slurmcommons::conf_dir_path"],
+    style      => 'keyval',
+    data       => $slurmcommons::slurm_conf_options,
+    exceptions => ['Include'],
+    require    => File[$slurmcommons::conf_dir_path],
   }
 
   hpclib::print_config { $slurmcommons::part_conf_file :
-    style        => 'linebyline',
-    data         => $slurmcommons::partitions_conf,
-    require      => File["$slurmcommons::conf_dir_path"],
+    style   => 'linebyline',
+    data    => $slurmcommons::partitions_conf,
+    require => File[$slurmcommons::conf_dir_path],
   }
 
 }
--- puppet-config/modules/slurmcommons/manifests/params.pp
+++ puppet-config/modules/slurmcommons/manifests/params.pp
@@ -1,53 +1,68 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class slurmcommons::params {
   ### Configuration ###
-  $bin_dir_path          = '/usr/lib/slurm'
-  $conf_dir_path         = '/etc/slurm-llnl'
-  $logs_dir_path         = '/var/log/slurm-llnl'
-  $script_dir_path       = "${bin_dir_path}/generic-scripts"
-  $main_conf_file        = "${conf_dir_path}/slurm.conf"
-  $part_conf_file        = "${conf_dir_path}/partitions.conf"
+  $bin_dir_path    = '/usr/lib/slurm'
+  $conf_dir_path   = '/etc/slurm-llnl'
+  $logs_dir_path   = '/var/log/slurm-llnl'
+  $script_dir_path = "${bin_dir_path}/generic-scripts"
+  $main_conf_file  = "${conf_dir_path}/slurm.conf"
+  $part_conf_file  = "${conf_dir_path}/partitions.conf"
   $slurm_conf_options = {
     'ClusterName' => {
-       value   => 'mycluster',
-       comment => 'The name by which this SLURM managed cluster is known in the accounting database',
-     },
-     'ControlMachine' => {
-       value   => 'localhost',
-       comment => 'Hostname of the machine where SLURM control functions are executed',
-     },
-     'SlurmUser' => {
-       value   => 'slurm',
-       comment => 'The name of the user that the slurmctld daemon executes as',
-     },
-     'SlurmctldPort' => {
-       value   => '6817',
-       comment => 'The port number that the SLURM controller, slurmctld, listens to for work',
-     },
-     'SlurmdPort' => {
-       value   => '6818',
-       comment => 'The port number that the SLURM compute node daemon, slurmd, listens to for work. SlurmctldPort and SlurmdPort must be different',
-     },
-     'SlurmctldPidFile' => {
-       value   => '/var/run/slurm-llnl/slurmctld.pid',
-       comment => 'File into which the slurmctld daemon may write its process id',
-     },
-     'SlurmdPidFile' => {
-       value   => '/var/run/slurm-llnl/slurmd.pid',
-       comment => 'File into which the slurmd daemon may write its process id',
-     },
-     'SlurmdSpoolDir' => {
-       value   => '/var/spool/slurmd',
-       comment => 'Directory (local file system) into which the slurmd daemons state information and batch job script information are written',
-     },
-     'AuthType' => {
-       value   => 'auth/munge',
-       comment => 'The authentication method for communications between SLURM components',
-     },
-     'CryptoType' => {
-       value   => 'crypto/munge',
-       comment => 'The cryptographic signature tool to be used in the creation of job step credentials',
-     },
-  }
+      value   => 'mycluster',
+      comment => 'The name by which this SLURM managed cluster is known in the accounting database',
+    },
+    'ControlMachine' => {
+      value   => 'localhost',
+      comment => 'Hostname of the machine where SLURM control functions are executed',
+    },
+    'SlurmUser' => {
+      value   => 'slurm',
+      comment => 'The name of the user that the slurmctld daemon executes as',
+    },
+    'SlurmctldPort' => {
+      value   => '6817',
+      comment => 'The port number that the SLURM controller, slurmctld, listens to for work',
+    },
+    'SlurmdPort' => {
+      value   => '6818',
+      comment => 'The port number that the SLURM compute node daemon, slurmd, listens to for work. SlurmctldPort and SlurmdPort must be different',
+    },
+    'SlurmctldPidFile' => {
+      value   => '/var/run/slurm-llnl/slurmctld.pid',
+      comment => 'File into which the slurmctld daemon may write its process id',
+    },
+    'SlurmdPidFile' => {
+      value   => '/var/run/slurm-llnl/slurmd.pid',
+      comment => 'File into which the slurmd daemon may write its process id',
+    },
+    'SlurmdSpoolDir' => {
+      value   => '/var/spool/slurmd',
+      comment => 'Directory (local file system) into which the slurmd daemons state information and batch job script information are written',
+    },
+    'AuthType' => {
+      value   => 'auth/munge',
+      comment => 'The authentication method for communications between SLURM components',
+    },
+    'CryptoType' => {
+      value   => 'crypto/munge',
+      comment => 'The cryptographic signature tool to be used in the creation of job step credentials',
+    },
+    }
 
   $partitions_conf = [
     'NodeName=localhost CPUs=1 State=UNKNOWN',
