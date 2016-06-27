e9d4fd3868041e8028c16954738dce52834369f7
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-27 14:09:44 +0100

    mod:boothttp Lint fixes

--- puppet-config/modules/boothttp/manifests/printconfig.pp
+++ puppet-config/modules/boothttp/manifests/printconfig.pp
@@ -1,13 +1,30 @@
-#
-define boothttp::printconfig ($os = 'calibre9') {
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
 
-  $config_file     = "${boothttp::install::disk_dir}/${os}/install_config"
+#
+define boothttp::printconfig (
+  $os = 'calibre9'
+) {
+
+  $config_file     = "${::boothttp::install::disk_dir}/${os}/install_config"
   $template_file   = "boothttp/install_config.${os}.erb"
   $virtual_address = $boothttp::virtual_address
 
-  file { "${config_file}":
-    ensure          => 'present',
-    content         => template($template_file)
+  file { $config_file:
+    ensure  => 'present',
+    content => template($template_file)
   }
 
 }
