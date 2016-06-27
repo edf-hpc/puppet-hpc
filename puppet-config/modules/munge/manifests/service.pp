14c9975af1488ea8737742d3bef7a3984f6e67c4
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-27 10:54:17 +0100

    mod:munge Lint fixes

--- puppet-config/modules/munge/manifests/service.pp
+++ puppet-config/modules/munge/manifests/service.pp
@@ -1,3 +1,18 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class munge::service {
 
   if $munge::service_manage == true {
@@ -7,9 +22,9 @@ class munge::service {
     }
 
     service { $munge::service_name :
-      ensure     => $munge::service_ensure,
-      enable     => $munge::service_enable,
-      name       => $munge::service_name,
+      ensure => $munge::service_ensure,
+      enable => $munge::service_enable,
+      name   => $munge::service_name,
     }
   }
 }
