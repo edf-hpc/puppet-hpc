6db53c39e13fd9c2279201da69d79dacf2135d70
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-27 14:12:41 +0100

    mod:environment Syntax typo

--- puppet-config/modules/environment/manifests/params.pp
+++ puppet-config/modules/environment/manifests/params.pp
@@ -68,7 +68,7 @@ class environment::params {
     'info'  => [  '',
                   "${::cluster} HPC System",
                   '',
-                  $::hostname},
+                  $::hostname,
                   '',
             ],
     'legal' => [ '' ],
