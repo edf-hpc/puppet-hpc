878ebe2255fde317ea1399c0934f67333b375d10
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-06-29 13:20:19 +0100

    mod:gpfs Syntax fix (Fixes #30)

--- puppet-config/modules/gpfs/manifests/params.pp
+++ puppet-config/modules/gpfs/manifests/params.pp
@@ -40,7 +40,9 @@ class gpfs::params {
         '7': {
           $cl_kernel         = ['gpfs.gpl-3.2.0-4-amd64']
         }
-        default: {}
+        default: {
+          fail("Unsupported OS major release '${::operatingsystemmajrelease}', should be: '6', '7'.")
+        }
       }
       $sr_packages           = []
       $sr_packages_ensure    = ''
@@ -60,31 +62,35 @@ class gpfs::params {
         '6': {
           $cl_kernel         = ['gpfs.gplbin-2.6.32-431.el6.x86_64']
         }
-        default: {}
+        default: {
+          fail("Unsupported OS major release '${::operatingsystemmajrelease}', should be: '6', '7'.")
+        }
       }
       $sr_packages           = ['gpfs.docs','set_dma_latency']
       $sr_packages_ensure    = 'present'
     }
-    default: {}
-    $cl_packages       = [$cl_base, $cl_kernel]
-    $cl_packages_ensure= 'present'
-    $cl_config_dir     = [
-      '/var/mmfs',
-      '/var/mmfs/gen',
-      '/var/lock',
-      '/var/lock/subsys',
-      '/usr/lpp',
-      '/usr/lpp/mmfs',
-      '/usr/lpp/mmfs/lib',
-      '/var/mmfs/ssl',
-      '/var/mmfs/ssl/stage',
-    ]
-    $cl_config         = '/var/mmfs/gen/mmsdrfs'
-    $cl_config_src     = ''
-    $cl_key            = '/var/mmfs/ssl/stage/genkeyData1'
-    $cl_key_src        = ''
-    $cl_perf           = '/usr/lpp/mmfs/samples/perf'
-    $cl_perf_src       = ''
+    default: {
+      fail("Unsupported OS Family '${::osfamily}', should be: 'Debian', 'Redhat'.")
+    }
   }
+  $cl_packages       = [$cl_base, $cl_kernel]
+  $cl_packages_ensure= 'present'
+  $cl_config_dir     = [
+    '/var/mmfs',
+    '/var/mmfs/gen',
+    '/var/lock',
+    '/var/lock/subsys',
+    '/usr/lpp',
+    '/usr/lpp/mmfs',
+    '/usr/lpp/mmfs/lib',
+    '/var/mmfs/ssl',
+    '/var/mmfs/ssl/stage',
+  ]
+  $cl_config         = '/var/mmfs/gen/mmsdrfs'
+  $cl_config_src     = ''
+  $cl_key            = '/var/mmfs/ssl/stage/genkeyData1'
+  $cl_key_src        = ''
+  $cl_perf           = '/usr/lpp/mmfs/samples/perf'
+  $cl_perf_src       = ''
 
 }
