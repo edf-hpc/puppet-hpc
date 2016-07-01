d091e6744ec99c50fbbdbb30536bce636d56d0fa
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-07-01 12:44:10 +0100

    mod:clara Module documentation

--- puppet-config/modules/clara/manifests/init.pp
+++ puppet-config/modules/clara/manifests/init.pp
@@ -13,6 +13,33 @@
 #  GNU General Public License for more details.                          #
 ##########################################################################
 
+# Install the clara cluster administration tool
+#
+# @param packages packages list
+# @param packages_ensure packages install mode
+# @param config_file Main configuration file path (`config.ini`)
+# @param repos_file Repository declaration file path (`repos.ini`)
+# @param repos_options Content of the `repos_file` in a hash
+# @param keyring_file Encoded keyring secret file
+#  (`cluster_keyring.secret.gpg.enc`) path 
+# @param keyring_source Encoded keyring secret file
+#  (`cluster_keyring.secret.gpg.enc`) source
+# @param password_file Master password file path
+# @param password_options Master password file content in a hash
+# @param common_options Content of the common section of `config.ini` in a hash
+#   , merged with default values
+# @param repo_options Content of the common section of `config.ini` in a hash
+#   , merged with default values
+# @param ipmi_options Content of the common section of `config.ini` in a hash
+#   , merged with default values
+# @param images_options Content of the common section of `config.ini` in a hash
+#   , merged with default values
+# @param p2p_options Content of the common section of `config.ini` in a hash
+#   , merged with default values
+# @param build_options Content of the common section of `config.ini` in a hash
+#   , merged with default values
+# @param slurm_options Content of the whole `config.ini` in a hash, merged with
+#   previous parameters
 class clara (
   $packages         = $::clara::params::packages,
   $packages_ensure  = $::clara::params::packages_ensure,
