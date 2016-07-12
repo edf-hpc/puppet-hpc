69ef9f507b8e3c4907ef02eef1d886edc9051661
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-07-12 15:50:36 +0100

    mod:nfs Add common nfs class

---
+++ puppet-config/modules/nfs/manifests/init.pp
@@ -0,0 +1,67 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# NFS common
+#
+# For NFSv4 it is mandatory to have a working idmapd configuration, the
+# most basic setup is for all the nodes to use the same idmapd domain.
+#
+# By default, idmapd will use the local domain name (`hostname -d`). If
+# you wish to override this setting you can add a `idmapd_options`:
+# ```
+# class{ '::nfs':
+#   idmapd_options => {
+#     'General' => {
+#       'Domain' => {
+#         'comment' => 'Domain for the mapping',
+#         'value'   => 'hpc.example.edu',
+#       },
+#     },
+#   },
+# }
+# ```
+#
+# @param idmapd_options    Content of the idampd configuration file
+# @param idmapd_file       Path of the idmapd configuration file
+# @param packages_ensure   Install mode (`latest` or `present`) for the
+#                          packages (default: `present`)
+# @param packages          Array of packages names
+# @param service_ensure    Ensure state of the service: `running` or
+#                          `stopped` (default: running)
+# @param service           Name of the service
+class nfs (
+  $idmapd_options  = {},
+  $idmapd_file     = $::nfs::params::idmapd_file,
+  $service_ensure  = $::nfs::params::service_ensure,
+  $service         = $::nfs::params::service,
+  $packages_ensure = $::nfs::params::packages_ensure,
+  $packages        = $::nfs::params::packages,
+) inherits ::nfs::params {
+  validate_absolute_path($idmapd_file)
+  validate_hash($idmapd_options)
+  validate_array($packages)
+  validate_string($packages_ensure)
+  validate_string($service)
+  validate_string($service_ensure)
+
+  $_idmapd_options = deep_merge($::nfs::params::idmapd_options_defaults, $idmapd_options)
+
+  anchor { 'nfs::begin': } ->
+  class { '::nfs::install': } ->
+  class { '::nfs::config': } ~>
+  class { '::nfs::service': } ->
+  anchor { 'nfs::end': }
+
+}
---
+++ puppet-config/modules/nfs/manifests/params.pp
@@ -0,0 +1,57 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class nfs::params {
+  # Module variables
+  $packages_ensure = 'present'
+  $service_ensure  = 'running'
+  case $::osfamily {
+    'Debian': {
+      $packages = ['nfs-common']
+      $service  = 'nfs-common'
+    }
+    'Redhat': {
+      $packages = ['nfs-utils.x86_64']
+      $service  = 'nfs'
+    }
+    default: {
+      fail("Unsupported OS Family: ${::osfamily}")
+    }
+  }
+
+  $idmapd_file = '/etc/idmapd.conf'
+  $idmapd_options_defaults = {
+    'General' => {
+      'Verbosity' => {
+        'comment'   => 'Verbosity',
+        'value'     => '0',
+      },
+      'Pipefs-Directory' => {
+        'comment'   => 'Piperfs-Directory',
+        'value'     => '/run/rpc_pipefs',
+      },
+    },
+    'Mapping' => {
+      'Nobody-User' => {
+        'comment'   => 'Nobody-User',
+        'value'     => 'nobody',
+      },
+      'Nobody-Group' => {
+        'comment'   => 'Nobody-Group',
+        'value'     => 'nogroup',
+      },
+    },
+  }
+}
