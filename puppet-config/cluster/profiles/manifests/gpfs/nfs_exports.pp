863525e86b0d38b2454d7f50ea604cec8bcbade8
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-07-19 14:30:41 +0100

    prof:gpfs Add profiles:: to hiera key (fixes #34)

--- puppet-config/cluster/profiles/manifests/gpfs/nfs_exports.pp
+++ puppet-config/cluster/profiles/manifests/gpfs/nfs_exports.pp
@@ -16,20 +16,20 @@
 # GPFS -  CNFS server
 #
 # ## Hiera
-# * `gpfs::nfs_to_export` (`hiera_hash`)
+# * `profiles::gpfs::nfs_to_export` (`hiera_hash`)
 class profiles::gpfs::nfs_exports {
 
   # Hiera lookups
-  $to_export = hiera_hash('gpfs::nfs_to_export')
+  $to_export = hiera_hash('profiles::gpfs::nfs_to_export')
 
   # Install gpfs client
-  class { '::gpfs::client': }
+  include ::gpfs::client
 
   # Install gpfs server
-  class { '::gpfs::server': }
+  include ::gpfs::server
 
   # Set up multipath
-  class { '::multipath': }
+  include ::multipath
 
   # Mount all the specified directories
   create_resources('::gpfs::server::export', $to_export)
