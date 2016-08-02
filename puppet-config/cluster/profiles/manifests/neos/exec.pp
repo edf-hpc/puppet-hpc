6c86a24b756731146df51edd981316caf08f7a78
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-08-02 16:01:38 +0100

    prof:neos Add new packages on exec and documentation

--- puppet-config/cluster/profiles/manifests/neos/exec.pp
+++ puppet-config/cluster/profiles/manifests/neos/exec.pp
@@ -15,10 +15,15 @@
 
 # Neos on an exec host
 #
+# On exec node, this profiles also install `neos-slurm-graphical-plugins`.
+# This package sets an epilog scripts that reset the node X server state.
+#
 # ## Hiera
-# * `profiles::neos::config_options` (`hiera_hash`)
+# * `profiles::neos::config_options` (`hiera_hash`) Content of the config
+#       file for '/etc/neos.conf'.
 class profiles::neos::exec {
   $packages = [
+    'neos-slurm-graphical-plugins',
     'xfce4',
     'x11vnc',
   ]
