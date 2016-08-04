5114de4a24c343b206145d8f6750605b1c8686e0
Thomas HAMEL <thomas.hamel@t-hamel.fr>
2016-08-04 17:52:46 +0200

    mod:libvirt Add new module for libvirt daemon and qemu

---
+++ puppet-config/modules/libvirt/manifests/params.pp
@@ -0,0 +1,35 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class libvirt::params {
+  #### Module variables
+  $packages        = [
+    'ebtables',
+    'dnsmasq',
+    'libvirt-bin',
+    'libvirt-daemon',
+    'netcat-openbsd',
+    'qemu-kvm',
+    'qemu-utils',
+  ]
+  $packages_ensure = installed
+  $service         = 'libvirtd'
+  $service_ensure  = running
+  $service_enable  = true
+  $config_file     = '/etc/libvirt/libvirtd.conf'
+
+  #### Default values
+  $config_options_defaults = { }
+}
