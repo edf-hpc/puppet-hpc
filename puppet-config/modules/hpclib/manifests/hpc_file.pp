cc58fdc7723e6d8b5dd4dc4356131143bc067ce7
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-08-09 09:54:36 +0200

    mod:hpclib Add a type that use hpc_source_file
    Can be used by a create resource from a hiera array this way.

---
+++ puppet-config/modules/hpclib/manifests/hpc_file.pp
@@ -0,0 +1,47 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# Create a file with an hpc_source_file content
+#
+# @param backup Save the file before it is replaced.
+# @param mode   Permissions mode of the file.
+# @param owner  User owner of the file.
+# @param target Path of the file to write.
+# @param source Source(s) of the file (absolute path or URL), can be a
+#               a single value or an array of values
+define hpclib::hpc_file(
+  $source,
+  $target          = $title,
+  $mode            = '0644',
+  $owner           = 'root',
+  $backup          = undef,
+) {
+
+  validate_string($mode)
+  validate_string($owner)
+  if $backup {
+    validate_bool($backup)
+  }
+  validate_absolute_path($target)
+
+  file { $target:
+    content => hpc_source_file($source),
+    mode    => $mode,
+    owner   => $owner,
+    backup  => $backup,
+  }
+
+}
+
