76deb6552e8f622ea237504eca079756b94d50f3
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-08-09 10:01:04 +0200

    prof:bootsystem Convert hiera profiles keys to autolookups

--- puppet-config/cluster/profiles/manifests/bootsystem/server.pp
+++ puppet-config/cluster/profiles/manifests/bootsystem/server.pp
@@ -19,12 +19,13 @@
 # * `cluster_prefix`
 # * `profiles::bootsystem::tftp_config_options` (`hiera_hash`)
 # * `profiles::bootsystem::tftp_dir`
-# * `profiles::bootsystem::ipxe_efi_source`
-# * `profiles::bootsystem::ipxe_leg_source`
 # * `profiles::bootsystem::http_dir`
-# * `profiles::bootsystem::menu_source`
-# * `profiles::bootsystem::http_disk_source`
 # * `profiles::bootsystem::supported_os`
+#
+# ## Relevant Autolookups
+# * `boothttp::menu_source`
+# * `boottftp::ipxe_efi_source`
+# * `boottftp::ipxe_legacy_source`
 class profiles::bootsystem::server {
 
   $prefix          = hiera('cluster_prefix')
@@ -38,27 +39,19 @@ class profiles::bootsystem::server {
   }
 
   # Install files necessary for boot
-  $config_dir_ftp      = hiera('profiles::bootsystem::tftp_dir')
-  $ipxe_efi_source     = hiera('profiles::bootsystem::ipxe_efi_source')
-  $ipxe_legacy_source  = hiera('profiles::bootsystem::ipxe_leg_source')
+  $config_dir_ftp     = hiera('profiles::bootsystem::tftp_dir')
 
   class { '::boottftp':
-    config_dir_ftp             => $config_dir_ftp,
-    ipxe_efi_source            => $ipxe_efi_source,
-    ipxe_legacy_source         => $ipxe_legacy_source,
+    config_dir_ftp     => $config_dir_ftp,
   }
 
-  $config_dir_http     = hiera('profiles::bootsystem::http_dir')
-  $menu_source         = hiera('profiles::bootsystem::menu_source')
-  $disk_source         = hiera('profiles::bootsystem::http_disk_source')
-  $supported_os        = hiera('profiles::bootsystem::supported_os')
+  $config_dir_http = hiera('profiles::bootsystem::http_dir')
+  $supported_os    = hiera('profiles::bootsystem::supported_os')
 
   class { '::boothttp':
-    config_dir_http            => $config_dir_http,
-    menu_source                => $menu_source,
-    disk_source                => $disk_source,
-    supported_os               => $supported_os,
-    virtual_address            => $virtual_address,
+    config_dir_http => $config_dir_http,
+    supported_os    => $supported_os,
+    virtual_address => $virtual_address,
   }
 
 }
