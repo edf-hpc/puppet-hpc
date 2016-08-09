adad46f1f66e6b16f17961627b3551f37418add5
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2016-08-09 10:03:03 +0200

    prof:dhcp Use internal hostname with domain as virtual address

--- puppet-config/cluster/profiles/manifests/dhcp/server.pp
+++ puppet-config/cluster/profiles/manifests/dhcp/server.pp
@@ -55,7 +55,7 @@ class profiles::dhcp::server {
     }
   }
   $_failover = merge($failover, $failover_add)
-  $virtual_address = $::hostfile["${prefix}${::puppet_role}"]
+  $virtual_address = "${prefix}${::puppet_role}.cluster"
 
   # Pass config options as a class parameter
   class { '::iscdhcp':
