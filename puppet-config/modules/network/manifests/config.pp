e5adcdd51587f0335a4099680253faacdd3b4b24
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-09-01 13:35:47 +0100

    Add hostfile entries with fqdn

--- puppet-config/modules/network/manifests/config.pp
+++ puppet-config/modules/network/manifests/config.pp
@@ -28,6 +28,17 @@ class network::config inherits network {
       upper_case_keys => true,
     }
   }
+  
+  host { 'localhost':
+    ensure => present,
+    ip     => '127.0.0.1',
+  }
+
+  host { $::network::fqdn:
+    ensure       => present,
+    ip           => '127.0.1.1',
+    host_aliases => $::hostname
+  }
 
   # $net_ifaces hash is used by create_resources to generate main network
   # configuration file. On debian systems there is a single file.
