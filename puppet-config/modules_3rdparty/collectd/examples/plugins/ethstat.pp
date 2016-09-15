9ff4b94fe4e1fe2c823cd9e542e2b9afa4083084
Rémi Palancher <remi-externe.palancher@edf.fr>
2016-09-15 12:22:16 +0200

    Remove almost all 3rd party modules
    Since they are now packaged externally except augeasprovider_pam that
    must stay here for the moment because of a bug in its package.

--- puppet-config/modules_3rdparty/collectd/examples/plugins/ethstat.pp
+++
@@ -1,6 +0,0 @@
-include ::collectd
-
-class { '::collectd::plugin::ethstat':
-  maps       => ['"rx_csum_offload_errors" "if_rx_errors" checksum_offload"', '"multicast" "if_multicast"'],
-  mappedonly => false,
-}
