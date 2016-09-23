40b1f4b83036ac6bffea67771c3b822ea9f27420
Rémi Palancher <remi-externe.palancher@edf.fr>
2016-09-23 18:21:43 +0200

    mod:hpclib: add functions for new hiera VIPs

---
+++ puppet-config/modules/hpclib/lib/puppet/parser/functions/hpc_ha_vservs.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2016 EDF S.A.                                           #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+require('hpc/ha')
+
+# @return A list of vserv hash for the current host
+Puppet::Parser::Functions::newfunction(
+  :hpc_ha_vservs,
+  :type  => :rvalue,
+  :arity => -1,
+  :doc   =>
+    "Return a list of vserv hash for the current host"
+) do |args|
+
+  result = hpc_ha_vservs()
+
+  begin
+    result
+  rescue Puppet::ParseError => internal_error
+    if internal_error.original.nil?
+      raise internal_error
+    else
+      raise internal_error.original
+    end
+  end
+end
