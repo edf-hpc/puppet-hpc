e393bd0088c66dc9873384d9c1bc78a07f3893d1
Mehdi Dogguy <mehdi.dogguy@edf.fr>
2016-09-23 18:09:48 +0200

    Add Ceph module

---
+++ puppet-config/modules/ceph/manifests/init.pp
@@ -0,0 +1,43 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class ceph (
+  $packages        = $::ceph::params::packages,
+  $packages_ensure = $::ceph::params::packages_ensure,
+  $service         = $::ceph::params::service,
+  $service_ensure  = $::ceph::params::service_ensure,
+  $service_enable  = $::ceph::params::service_enable,
+  $config_file     = $::ceph::params::config_file,
+  $config_options  = {},
+  $keyrings        = {},
+) inherits ceph::params {
+
+  validate_array($packages)
+  validate_string($packages_ensure)
+  validate_string($service)
+  validate_string($service_ensure)
+  validate_bool($service_enable)
+  validate_absolute_path($config_file)
+  validate_hash($config_options)
+  validate_hash($keyrings)
+
+  $_config_options = merge ($::ceph::params::config_options_defaults, $config_options)
+
+  anchor { 'ceph::begin': } ->
+  class { '::ceph::install': } ->
+  class { '::ceph::config': } ->
+  class { '::ceph::service': } ->
+  anchor { 'ceph::end': }
+}
