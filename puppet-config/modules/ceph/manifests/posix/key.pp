6926733234925f877e6b31c3dc82392b73974a43
Rémi Palancher <remi-externe.palancher@edf.fr>
2016-09-29 20:11:14 +0200

    mod:ceph rename fsmount to posix::client
    The new posix::client deploys all the basic stuff required by a Ceph
    POSIX clients to mount filesystems. There 2 new defined types:
    
    - ceph::posix::key
    - ceph::posix::mount
    
    Those defined types are respectively called by the
    ceph::posix::client::keys and ceph::posix::client::mounts classes with
    the hashes given in parameters of the ceph::posix::client public class.

---
+++ puppet-config/modules/ceph/manifests/posix/key.pp
@@ -0,0 +1,32 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# Mount a POSIX Ceph FS using kernel module
+#
+# @param key        The secret key file
+define ceph::posix::key (
+  $key
+)
+{
+    $_keyfile = "${::ceph::posix::client::keys_dir}/${name}.key"
+
+    file { $_keyfile:
+      content => $key,
+      owner   => $::ceph::posix::client::keys_owner,
+      group   => $::ceph::posix::client::keys_group,
+      mode    => $::ceph::posix::client::keys_mode,
+    }
+
+}
