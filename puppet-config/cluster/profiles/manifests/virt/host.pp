2a894fddcd71f114559814359a7080475b6c5a9e
Thomas Hamel <thomas-externe.hamel@edf.fr>
2016-10-03 10:52:08 +0200

    prod:virt Load pools, networks and secrets from hiera and create resources

--- puppet-config/cluster/profiles/manifests/virt/host.pp
+++ puppet-config/cluster/profiles/manifests/virt/host.pp
@@ -17,6 +17,20 @@
 #
 # Will install a libvirt daemon with kvm. No other configuration (hosts,
 # networks, pools, volumes...) done by default.
+#
+# ## Hiera
+# * profiles::virt::networks (`hiera_hash`) Resource definitions for libvirt::network
+# * profiles::virt::pools (`hiera_hash`) Resource definitions for libvirt::pool
+# * profiles::virt::secrets (`hiera_hash`) Resource definitions for libvirt::secret
 class profiles::virt::host {
   include '::libvirt'
+
+  $networks = hiera_hash('profiles::virt::networks')
+  $pools = hiera_hash('profiles::virt::pools')
+  $secrets = hiera_hash('profiles::virt::secrets')
+
+  create_resources(libvirt::network, $networks)
+  create_resources(libvirt::pool, $pools)
+  create_resources(libvirt::secret, $secrets)
+
 }
