f6a0e7eb8a9a92db70b745094c5a1e20fe7d9f36
Rémi Palancher <remi-externe.palancher@edf.fr>
2016-10-05 10:00:05 +0200

    mod:slurm ensure backup dir also exist
    And manage properly the case where the archive and backup directories
    both share the same path.

--- puppet-config/modules/slurm/manifests/dbd/install.pp
+++ puppet-config/modules/slurm/manifests/dbd/install.pp
@@ -23,23 +23,39 @@ class slurm::dbd::install inherits slurm::dbd {
 
   }
 
-  # To create the archive directory, we extract its path from the slurmdbd
-  # configuration hash. This hash only exists if dbd::config_manage is true,
+  # To create the archive and DB backup directories, we extract theirs paths
+  # respectively from the slurmdbd configuration and the db backup options
+  # hashes. These hashes are defined only if dbd::config_manage is true,
   # hence this test.
+
   if $::slurm::dbd::config_manage {
 
-    $_archive_dir = $::slurm::dbd::_config_options['ArchiveDir']
     $_slurm_user = $::slurm::dbd::_config_options['SlurmUser']
 
-    # If the ArchiveDir is not defined in slurmdbd configuration, do nothing.
+    # A common set of resources properties are defined to make sure both
+    # archive and backup directories are created with the same properties and
+    # avoid a resource conflict with ensure_resource() in case they share the
+    # same path.
 
+    $_dir_props = {
+      ensure => directory,
+      owner  => $_slurm_user,
+      group  => $_slurm_user,
+      mode   => 0755,
+    }
+
+
+    # If the ArchiveDir is not defined in slurmdbd configuration, do nothing.
+    $_archive_dir = $::slurm::dbd::_config_options['ArchiveDir']
     if $_archive_dir != undef {
-      file { $_archive_dir:
-        ensure => directory,
-        owner  => $_slurm_user,
-        group  => $_slurm_user,
-        mode   => 0755,
-      }
+      ensure_resource('file', $_archive_dir, $_dir_props)
+    }
+
+    # The _db_backup_option hash is defined only if db_backup_enable is true,
+    # hence this additional test.
+    if $::slurm::dbd::db_backup_enable {
+      $_backup_dir = $::slurm::dbd::_db_backup_options['BKDIR']
+      ensure_resource('file', $_backup_dir, $_dir_props)
     }
   }
 }
