29c728d831171ff10c4393b4ccc85a601e3ce195
Mehdi Dogguy <mehdi.dogguy@edf.fr>
2016-10-07 14:53:18 +0200

    First configuration for Ceph services

--- puppet-config/modules/ceph/manifests/service.pp
+++ puppet-config/modules/ceph/manifests/service.pp
@@ -14,10 +14,53 @@
 ##########################################################################
 
 class ceph::service inherits ceph {
-  $services += [ "ceph-osd@${::ceph::osd_config[$::hostname]['id']}" ]
+  if $::ceph::service_enable {
 
-  service { $services :
+  if has_key($::ceph::osd_config, $::hostname) {
+    $osd = sprintf($::ceph::osd_service, $::ceph::osd_config[$::hostname]['id'])
+    $b_osd = basename($osd)
+    hpclib::systemd_meta_service { $b_osd :
+      service => $osd,
+      source  => $::ceph::osd_meta_service,
+      ensure  => $::ceph::service_ensure,
+      enable => $::ceph::service_enable,
+    }
+  }
+
+  if $::hostname in $::ceph::mds_config {
+    $b_mds = basename($::ceph::mds_service)
+    hpclib::systemd_meta_service { $b_mds :
+      service => $::ceph::mds_service,
+      source  => $::ceph::mds_meta_service,
+      ensure  => $::ceph::service_ensure,
+      enable => $::ceph::service_enable,
+    }
+  }
+
+  if $::hostname in $::ceph::mon_config {
+    $b_mon = basename($::ceph::mon_service)
+    hpclib::systemd_meta_service { $b_mon :
+      service => $::ceph::mon_service,
+      source  => $::ceph::mon_meta_service,
+      ensure  => $::ceph::service_ensure,
+      enable => $::ceph::service_enable,
+    }
+  }
+
+  if $::hostname in $::ceph::rgw_config {
+    $b_rgw = basename($::ceph::rgw_service)
+    hpclib::systemd_meta_service { $b_rgw :
+      service => $::ceph::rgw_service,
+      source  => $::ceph::rgw_meta_service,
+      ensure  => $::ceph::service_ensure,
+      enable => $::ceph::service_enable,
+    }
+  }
+
+  service { $::ceph::services :
     ensure => $::ceph::service_ensure,
     enable => $::ceph::service_enable,
+  }
+
   }
 }
