ce1602b66f8989a1182b35827694e4c08f353c2a
Cecile Yoshikawa <cecile.yoshikawa@edf.fr>
2016-10-12 17:54:17 +0200

    mod:puppet Add Puppet module to disable Puppet service

---
+++ puppet-config/modules/puppet/manifests/init.pp
@@ -0,0 +1,27 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class puppet (
+  $service          = $::puppet::params::service,
+  $service_ensure   = $::puppet::params::service_ensure,
+  $service_enable   = $::puppet::params::service_enable,
+) inherits puppet::params {
+  validate_string($service)
+  validate_string($service_ensure)
+  validate_bool($service_enable)
+
+  class { '::puppet::service': }
+
+}
