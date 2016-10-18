2470c6dd5192e541c1721831636ffdf6b3896362
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-10-18 11:44:23 +0200

    mod:glpicollector: New module to collect inventories and send them to a glpi server.

---
+++ puppet-config/modules/glpicollector/manifests/init.pp
@@ -0,0 +1,27 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class glpicollector (
+  $hpc_files = $glpicollector::params::hpc_files,
+) inherits glpicollector::params {
+
+  validate_hash($hpc_files)
+
+  anchor { 'glpicollector::begin': } ->
+  class { '::glpicollector::install': } ->
+  class { '::glpicollector::config': } ->
+  anchor { 'glpicollector::end': }
+
+}
