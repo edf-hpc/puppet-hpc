76ab7460e0f6996c2a5b42812639f3cb831b0d46
Thomas Hamel <thomas-externe.hamel@edf.fr>
2016-10-18 19:24:17 +0200

    mod:pstate A simple module to set pstate tuning
    Just set a script and run it during boot with a systemd unit.

---
+++ puppet-config/modules/pstate/files/pstate_set.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+##########################################################################
+#  Set pstate tuning                                                     #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# Disable turbo
+echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+# Using performance instead of powersave would reactivate
+# the turbo.
+for c in  /sys/devices/system/cpu/cpu*/cpufreq; do
+    echo powersave >  $c/scaling_governor
+done
+# Force the pstate to always be at the max frequency
+echo 100 > /sys/devices/system/cpu/intel_pstate/max_perf_pct
+echo 100 > /sys/devices/system/cpu/intel_pstate/min_perf_pct
