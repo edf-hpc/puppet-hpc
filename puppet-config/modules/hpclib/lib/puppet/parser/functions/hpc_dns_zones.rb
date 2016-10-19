14fea6aaf6a5ed05fca97f541222fa7b8b8d706d
Rémi Palancher <remi-externe.palancher@edf.fr>
2016-10-19 09:28:23 +0200

    mod:hpclib add dns function
    A new function hpc_dns_zones() can be used by profiles to extract out
    of hieradata a hash of zones (including reverse) to be directly given
    in parameters of dns::server::zone defined type.

---
+++ puppet-config/modules/hpclib/lib/puppet/parser/functions/hpc_dns_zones.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2016 EDF S.A.                                           #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+require('hpc/dns')
+
+# @return A hash of DNS zones hashes of the cluster
+Puppet::Parser::Functions::newfunction(
+  :hpc_dns_zones,
+  :type  => :rvalue,
+  :arity => -1,
+  :doc   =>
+    "Return a hash of DNS zones hashes of the cluster"
+) do |args|
+
+  result = hpc_dns_zones()
+
+  begin
+    result
+  rescue Puppet::ParseError => internal_error
+    if internal_error.original.nil?
+      raise internal_error
+    else
+      raise internal_error.original
+    end
+  end
+end
