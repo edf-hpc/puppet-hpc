365c07b80a0eb333bb6f9c334918bad65ded3210
Cecile Yoshikawa <cecile.yoshikawa@edf.fr>
2016-10-27 16:42:09 +0200

    mod:hidepid prof:environment Add the possibility to hide processes to other users

---
+++ puppet-config/cluster/profiles/manifests/environment/userspace.pp
@@ -0,0 +1,26 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# Install the environment necessary for any node in the userspace
+#
+class profiles::environment::userspace {
+
+  ## Hide processes from other users
+  ## Hiera lookups
+  $to_mount = hiera_hash('profiles::environment::userspace::proc_hidepid')
+  
+  create_resources('::hidepid::mount', $to_mount)
+
+}
