d648c9b60c095b9fd7c70bc868e5240db3356a53
Cecile Yoshikawa <cecile.yoshikawa@edf.fr>
2016-10-28 11:38:14 +0200

    mod:hidepid Execute the remount through systemd to deal with dependies at boot

---
+++ puppet-config/modules/hidepid/manifests/init.pp
@@ -0,0 +1,52 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+class hidepid (
+  $hidepid              = $hidepid::params::hidepid,
+  $gid                  = $hidepid::params::gid,
+  $service              = $hidepid::params::service,
+  $systemd_service_file = $hidepid::params::systemd_service_file,
+) inherits hidepid::params {
+
+  validate_string($hidepid)
+  validate_string($gid)
+
+  # Define the proper options 
+  if $gid == '' {
+    $options = "hidepid=${hidepid}"
+  } else {
+    $options = "hidepid=${hidepid},gid=${gid}"
+  }
+  $command  = "/bin/mount -o remount,${options} /proc"
+  $systemd_service_file_options = {
+    'Unit'    => {
+      'Description' => 'Remount proc with hidepid option',
+      'After'       => 'sssd.service',
+    },
+    'Service' => {
+      'Type'        => 'oneshot',
+      'ExecStart'   => $command,
+    },
+    'Install' => {
+      'WantedBy'    => 'multi-user.target',
+    },
+  }
+
+  anchor { 'hidepid::begin':} ->
+  class { '::hidepid::install': } ->
+  class { '::hidepid::service': } ->
+  anchor { 'hidepid::end': }
+
+}
