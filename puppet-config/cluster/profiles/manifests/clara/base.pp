1a961343ad9fc71f7a21450f55385e256c651efc
Benoit Boccard <benoit-externe.boccard@edf.fr>
2016-11-09 15:50:45 +0100

    bugfix : can't use certificate class as it's already used by auth::client profile

--- puppet-config/cluster/profiles/manifests/clara/base.pp
+++ puppet-config/cluster/profiles/manifests/clara/base.pp
@@ -17,11 +17,38 @@
 #
 # ## Hiera
 # * `profiles::clara::repos`
-
-include certificates
+# * `profiles::clara::ssl_directory_source`
+# * `profiles::clara::certificates_directory`
+# * `profiles::clara::certificate_file`
+# * `profiles::clara::key_file`
 
 class profiles::clara::base {
+
+  # Hiera lookups 
+  $directory_source       = hiera('profiles::clara::ssl_directory_source')
+  $certificates_directory = hiera('profiles::clara::certificates_directory')
+  $certificate_file       = hiera('profiles::clara::certificate_file')
+  $key_file               = hiera('profiles::clara::key_file')
+  $decrypt_passwd         = hiera('profiles::clara::decrypt_passwd')
+
   class { '::clara':
     repos_options => hiera_hash('profiles::clara::repos'),
   }
+
+  file { $certificates_directory :
+    ensure => 'directory',
+  }
+  file { "${certificates_directory}/${certificate_file}" :
+    ensure  => present,
+    content => decrypt("${directory_source}/${certificate_file}.enc", $decrypt_passwd),
+    require => File[$certificates_directory],
+    mode    => '0600',
+  }
+  file { "${certificates_directory}/${key_file}" :
+    ensure  => present,
+    content => decrypt("${directory_source}/${key_file}.enc", $decrypt_passwd),
+    require => File[$certificates_directory],
+    mode    => '0600',
+  }
+
 }
