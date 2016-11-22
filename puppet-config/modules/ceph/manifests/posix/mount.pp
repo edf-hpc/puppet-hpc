388fa7167669423caa95cd09056f0395b2d7679c
Thomas Hamel <thomas-externe.hamel@edf.fr>
2016-11-22 15:52:47 +0100

    mod:ceph Add support for FUSE mounts

--- puppet-config/modules/ceph/manifests/posix/mount.pp
+++ puppet-config/modules/ceph/manifests/posix/mount.pp
@@ -13,36 +13,58 @@
 #  GNU General Public License for more details.                          #
 ##########################################################################
 
-# Mount a POSIX Ceph FS using kernel module
+# Mount a POSIX Ceph FS
 #
-# @param servers    Array of Ceph MON servers
+# When using the kernel module, servers must be provided here. The FUSE
+# mode uses the configuration in /etc/ceph/ceph.conf.
+#
+# @param servers    Array of Ceph MON servers (not supported in fuse mode)
 # @param device     Directory to mount on the server
 # @param mountpoint Local mount point
 # @param user       Name of RADOS user
 # @param key        Name of the secret key file inside the keys directory
+# @param mode       Kernel or FUSE mode (default: 'kernel')
 define ceph::posix::mount (
-  $servers    = [ 'localhost' ],
+  $servers    = undef,
+  $key        = undef,
   $device     = '/',
   $mountpoint = '/mnt',
   $user       = 'admin',
-  $key,
+  $mode       = 'kernel',
+  $ceph_file  = '/etc/ceph/ceph.conf',
 ){
 
-  $_keyfile = "${::ceph::posix::client::keys_dir}/${key}.key"
+  if $mode == 'kernel' {
+    validate_string($key)
+    validate_array($servers)
+    $_keyfile = "${::ceph::posix::client::keys_dir}/${key}.key"
 
-  $_device = sprintf("%s:%s", join($servers, ','), $device)
-  $_options = sprintf("name=%s,secretfile=%s,_netdev", $user, $_keyfile)
+    $_device = sprintf("%s:%s", join($servers, ','), $device)
+    $_options = sprintf("name=%s,secretfile=%s,_netdev", $user, $_keyfile)
+    $_fstype = 'ceph'
+    $_remounts = true
+
+  } else {
+    if $servers {
+      warning("Servers supplied in FUSE mode will be ignored (configured by ${ceph_file}).")
+    }
+
+    $_device = "id=${user},client_mountpoint=${device}"
+    $_options = '_netdev'
+    $_fstype = 'fuse.ceph'
+    $_remounts = false
+  }
 
   file { $mountpoint:
     ensure => directory,
   }
 
   mount { $mountpoint:
-    ensure  => mounted,
-    fstype  => 'ceph',
-    device  => $_device,
-    options => $_options,
-    require => File[$mountpoint],
+    ensure   => mounted,
+    fstype   => $_fstype,
+    device   => $_device,
+    options  => $_options,
+    remounts => $_remounts,
+    require  => File[$mountpoint],
   }
-
 }
