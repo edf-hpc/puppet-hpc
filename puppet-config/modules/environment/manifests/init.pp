3416e9be2c7484cf096a4bd8d2158cb48cff22fc
Thomas Hamel <thomas-externe.hamel@edf.fr>
2016-12-01 11:34:46 +0100

    mod:environment Doc, lint fix and bump default key to 2048
    2048 is the default these days.

--- puppet-config/modules/environment/manifests/init.pp
+++ puppet-config/modules/environment/manifests/init.pp
@@ -13,6 +13,42 @@
 #  GNU General Public License for more details.                          #
 ##########################################################################
 
+# Setup standard user environment
+#
+# This class sets up:
+# - system shell configuration (/etc/bash.bashrc and /etc/profile.d)
+# - systemd-user-session
+# - ssh keys generation script for users
+# - MOTD
+#
+# # MOTD
+# The MOTD is in two parts that can be modified independently: info and legal.
+# The content is provided as a hash in the `motd_content` parameter.
+#
+# ```
+# motd_content => {
+#   'info' => [
+#     'Welcome to this System',
+#     $::hostname,
+#   ],
+#   'legal' => [
+#     'No Trespassing',
+#   ],
+# }
+# ```
+#
+# @param service_user_session Path of the user session service unit (default:
+#           '/etc/systemd/system/systemd-user-sessions.service')
+# @param service_user_session_options Hash with content of the user session
+#           service unit
+# @param motd_content Hash with the MOTD content (see above)
+# @param authorized_users_groups Comma separated list of groups that can connect
+#           to this node.
+# @param autogen_key_type Name of the type of SSH key generated when users logs (
+#           default: 'rsa')
+# @param autogen_key_length String representing the length of the generated key in
+#           bytes (default: '2048')
+# @param cluster Name of the cluster used in the SSH key names.
 class environment (
   $service_user_session         = $environment::params::service_user_session,
   $service_user_session_options = $environment::params::service_user_session_options,
@@ -26,7 +62,7 @@ class environment (
   validate_string($service_user_session)
   validate_hash($service_user_session_options)
   validate_hash($motd_content)
-  validate_string($authorized_users_group)
+  validate_string($authorized_users_groups)
   validate_string($autogen_key_type)
   validate_string($autogen_key_length)
 
