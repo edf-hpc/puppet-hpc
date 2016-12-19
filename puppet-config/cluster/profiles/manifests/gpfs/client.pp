76710635adaf7f6dc7fea82207fe342f43a7c6ef
Rémi Palancher <remi-externe.palancher@edf.fr>
2016-12-19 12:42:19 +0100

    prof:gpfs: GPFS client after NFS mounts

--- puppet-config/cluster/profiles/manifests/gpfs/client.pp
+++ puppet-config/cluster/profiles/manifests/gpfs/client.pp
@@ -28,4 +28,14 @@ class profiles::gpfs::client {
     cluster => $cluster,
   }
 
+  # Make sure all potential NFS mount have been realized before realizing the
+  # GPFS client class. When GPFS client starts, it first add a line to fstab
+  # which is expected by the mount command that it is run afterwhile (~3s). It
+  # seems there is a (yet unexplained) "conflict" with Puppet modifications of
+  # fstab happening with the Mount resources of the nfs module in the meantime.
+  # The observation was that the GPFS line vanished from the fstab when the GPFS
+  # mount command is run. To avoid this conflicting corner case, make sure the
+  # potential NFS mounts are processed before the GPFS client class.
+  Mount <| tag == 'nfs' |> -> Class['::gpfs::client']
+
 }
