8d69659036d9155569b9b023e8312c6052dfa6a8
EDF-HPC <dsp-cspite-ccn-hpc@edf.fr>
2016-12-22 13:51:33 +0100

    mod:aufs Remove udba=reval from aufs mounts parameters

--- puppet-config/modules/aufs/manifests/config.pp
+++ puppet-config/modules/aufs/manifests/config.pp
@@ -17,8 +17,8 @@ class aufs::config inherits aufs {
   # Mounts are defined but not activated, this is done by the service.
   #   x-systemd.requires-mount-for: systemctl start will refuse to start if mount for this
   #                                 path are not there
-  $usr_bin_options = "noauto,udba=reval,br=${::aufs::overlay_dir}/usr/bin:${::aufs::squashfs_dir}/usr/bin:${::aufs::chroot_dir}/usr/bin,x-systemd.requires-mount-for=${::aufs::chroot_dir}"
-  $usr_share_options = "noauto,udba=reval,br=${::aufs::overlay_dir}/usr/share:${::aufs::squashfs_dir}/usr/share:${::aufs::chroot_dir}/usr/share,x-systemd.requires-mount-for=${::aufs::chroot_dir}"
+  $usr_bin_options = "noauto,br=${::aufs::overlay_dir}/usr/bin:${::aufs::squashfs_dir}/usr/bin:${::aufs::chroot_dir}/usr/bin,x-systemd.requires-mount-for=${::aufs::chroot_dir}"
+  $usr_share_options = "noauto,br=${::aufs::overlay_dir}/usr/share:${::aufs::squashfs_dir}/usr/share:${::aufs::chroot_dir}/usr/share,x-systemd.requires-mount-for=${::aufs::chroot_dir}"
 
   mount { '/usr/bin':
     ensure  => present,
