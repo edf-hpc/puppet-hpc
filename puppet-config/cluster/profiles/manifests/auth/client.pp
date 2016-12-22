381f166abda8bd3b705067e63f3b05cb9a64b769
Rémi Palancher <remi-externe.palancher@edf.fr>
2016-12-22 11:17:48 +0100

    prof:auth: LDAP URI is now inserted in SSSD opts
    The LDAP servers URI is now inserted in SSSD options based on the value
    of boolean parameter external_ldap.

--- puppet-config/cluster/profiles/manifests/auth/client.pp
+++ puppet-config/cluster/profiles/manifests/auth/client.pp
@@ -16,14 +16,19 @@
 # Authentication system client
 #
 # Authentication uses the SSS daemon to configure node directory and
-# authentication. The common pattern supported by this profile is a local
-# LDAP replica and an external Kerberos server for authentication.
+# authentication. The profile can configure SSSD to authenticate against either
+# an internal LDAP replica or an external reference LDAP server. The optional
+# Kerberos configuration necessarily connect to an external server.
 #
 # ## SSSD
 #
 # SSSD is configured with four hiera keys
 # (`profiles::auth::client::sssd_options_*`), those options are combined
 # to create `sssd_options` used to generate the file `/etc/sssd/sssd.conf`
+#
+# The URI to the LDAP server depends on the value of the boolean `external_ldap`
+# parameter. If true, the URI is the value of `ldap_external_uri`, otherwise
+# (default) it is `ldap_internal_uri`.
 #
 # ## Kerberos
 #
@@ -45,9 +50,10 @@
 # profiles::auth::client::enable_kerberos: false
 # kdc_primary:                    'kdc1.example.com'
 # kdc_secondary:                  'kdc2.example.com'
-# external_ldap:                  'ldap.example.com'
+# ldap_external:                  'ldap.example.com
 # krb5_realm:                     'EXAMPLE.COM'
 #
+# profiles::auth::client::external_ldap: false
 # profiles::auth::client::sssd_options_general:
 #   config_file_version:  '2'
 #   reconnection_retries: '3'
@@ -61,6 +67,8 @@
 # profiles::auth::client::sssd_options_pam:
 #   reconnection_retries:           '3'
 #   offline_credentials_expiration: '1'
+# profiles::auth::client::ldap_external_uri: "ldaps://%{hiera('ldap_external_primary')}, ldaps://%{hiera('ldap_external_secondary')}"
+# profiles::auth::client::ldap_internal_uri: "ldaps://%{hiera('ldap_internal_primary')}, ldaps://%{hiera('ldap_internal_secondary')}"
 # profiles::auth::client::sssd_options_domain:
 #   name:                      "%{hiera('cluster_name')}"
 #   description:               "LDAP replica for %{hiera('cluster_name')}"
@@ -69,7 +77,6 @@
 #   cache_credentials:         'true'
 #   enumerate:                 'true'
 #   min_id:                    '1000'
-#   ldap_uri:                  "ldaps://%{hiera('cluster_prefix')}%{my_auth_replica}1, ldaps://%{hiera('cluster_prefix')}%{my_auth_replica}2"
 #   ldap_tls_reqcert:          'allow'
 #   ldap_tls_cacert:           "%{hiera('certificates::certificates_directory')}"
 #   ldap_search_timeout:       '3'
@@ -101,8 +108,8 @@
 #       {
 #         kdc = %{hiera('kdc_primary')}
 #         kdc = %{hiera('kdc_secondary')}
-#         admin_server = %{hiera('external_ldap')}
-#         kpasswd_server = %{hiera('external_ldap')}
+#         admin_server = %{hiera('ldap_external')}
+#         kpasswd_server = %{hiera('ldap_external')}
 #         default_domain = example.com
 #       }
 #   domain_realm:
@@ -123,6 +130,9 @@
 # * `profiles::auth::client::sssd_options_nss` (`hiera_hash`)
 # * `profiles::auth::client::sssd_options_pam` (`hiera_hash`)
 # * `profiles::auth::client::sssd_options_domain` (`hiera_hash`)
+# * `profiles::auth::client::external_ldap` (default value: false)
+# * `profiles::auth::client::ldap_external_uri`
+# * `profiles::auth::client::ldap_internal_uri`
 # * `profiles::auth::client::krb5_options` (`hiera_hash`)
 #
 # ## Relevant Autolookup
@@ -139,6 +149,21 @@ class profiles::auth::client {
   $krb5_options         = hiera_hash('profiles::auth::client::krb5_options')
   $enable_kerberos      = hiera('profiles::auth::client::enable_kerberos')
   $cluster              = hiera('cluster_name')
+  $external_ldap        = hiera('profiles::auth::client::external_ldap', false)
+
+  # Get external or internal LDAP URI depending on external_ldap boolean value
+  if $external_ldap == true {
+    $ldap_uri = hiera('profiles::auth::client::ldap_external_uri')
+  } else {
+    $ldap_uri = hiera('profiles::auth::client::ldap_internal_uri')
+  }
+
+  # insert ldap_uri into sssd_options_domain hash
+  $_sssd_options_domain_uri = {
+    ldap_uri => $ldap_uri,
+  }
+  $_sssd_options_domain = merge($sssd_options_domain,
+                                $_sssd_options_domain_uri)
 
   if $enable_kerberos {
     $sssd_options_domain_kerberos_opts = {
@@ -161,7 +186,8 @@ class profiles::auth::client {
     sssd                => $sssd_options_general,
     nss                 => $sssd_options_nss,
     pam                 => $sssd_options_pam,
-    "domain/${cluster}" => merge($sssd_options_domain,$sssd_options_domain_kerberos_opts),
+    "domain/${cluster}" => merge($_sssd_options_domain,
+                                 $sssd_options_domain_kerberos_opts),
   }
 
   include certificates
