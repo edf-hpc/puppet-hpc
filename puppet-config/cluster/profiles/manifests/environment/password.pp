471ce4e3c156ac196c3625d9249b18bf8fbb3927
Thomas Hamel <thomas-externe.hamel@edf.fr>
2017-01-06 18:24:46 +0100

    prof:environment Move password profile from access

--- puppet-config/cluster/profiles/manifests/environment/password.pp
+++ puppet-config/cluster/profiles/manifests/environment/password.pp
@@ -1,7 +1,7 @@
 ##########################################################################
 #  Puppet configuration file                                             #
 #                                                                        #
-#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Copyright (C) 2014-2017 EDF S.A.                                      #
 #  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
 #                                                                        #
 #  This program is free software; you can redistribute in and/or         #
@@ -13,21 +13,9 @@
 #  GNU General Public License for more details.                          #
 ##########################################################################
 
-# Pam_access configuration with `passwd` command support
-#
-# ## Hiera
-# * `profiles::access::access_config_opts` (`hiera_array`)
-class profiles::access::password {
+# Settings for ``password`` command
+class profiles::environment::password {
 
-  ## Hiera lookups
-
-  $access_config_opts = hiera_array('profiles::access::access_config_opts')
-
-  # Pass config options as a class parameter
-  include pam
-  class { '::pam::access':
-    access_config_opts => $access_config_opts,
-  }
   include pam::pwquality
 
 }
