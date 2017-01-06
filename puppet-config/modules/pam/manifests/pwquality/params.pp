2a744388edcb2fc7683b51acf93f209b5ba0437a
Thomas Hamel <thomas-externe.hamel@edf.fr>
2017-01-06 18:27:53 +0100

    mod:pam Debug pwquality
    Actually tested

--- puppet-config/modules/pam/manifests/pwquality/params.pp
+++ puppet-config/modules/pam/manifests/pwquality/params.pp
@@ -1,7 +1,7 @@
 ##########################################################################
 #  Puppet paramsuration file                                             #
 #                                                                        #
-#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Copyright (C) 2014-2017 EDF S.A.                                      #
 #  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
 #                                                                        #
 #  This program is free software; you can redistribute in and/or         #
@@ -16,7 +16,11 @@
 class pam::pwquality::params {
 
   $packages_ensure    = 'present'
-  $packages           = ['libpam-pwquality']
+  # Cracklib-runtime is used to get dictionaries
+  $packages           = [
+    'cracklib-runtime',
+    'libpam-pwquality',
+  ]
   $pamauthupdate_file = '/usr/share/pam-configs/pwquality'
 
 }
