2b12acb79db552603a547e51689fe90b8da13b98
Benoit Boccard <benoit-externe.boccard@edf.fr>
2017-01-06 11:23:35 +0100

    Prof: Add s3::s3cmd profile

---
+++ puppet-config/cluster/profiles/manifests/s3/s3cmd.pp
@@ -0,0 +1,27 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# S3cmd setup 
+#
+# ## Relevant Autolookup
+# * `s3cmd::config_options`  Parameters used to build config file for S3cmd
+class profiles::s3::s3cmd {
+
+
+# Install S3cmd software
+  class { '::s3cmd':
+  }
+
+}
