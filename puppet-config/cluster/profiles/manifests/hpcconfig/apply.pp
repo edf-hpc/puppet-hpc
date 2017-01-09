2c9019cec8b9af451abbde017f942acc61458cf0
Benoit Boccard <benoit-externe.boccard@edf.fr>
2017-01-09 16:30:54 +0100

    Add hpcconfig profiles

---
+++ puppet-config/cluster/profiles/manifests/hpcconfig/apply.pp
@@ -0,0 +1,27 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2016 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# Hpc-config-apply setup 
+#
+# ## Relevant Autolookup
+# * `hpcconfig::apply::config_options`  Parameters used to build config file for hpc-config-apply
+class profiles::hpcconfig::apply {
+
+
+# Install hpc-config-apply
+  class { '::hpcconfig::apply':
+  }
+
+}
