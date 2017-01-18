3bea3170e5de3c52eb43711ef3d02ddf02fa5fb4
Rémi Palancher <remi-externe.palancher@edf.fr>
2017-01-18 15:47:23 +0100

    prof:log: use new hpc_rsyslog module for server

--- puppet-config/cluster/profiles/manifests/log/server.pp
+++ puppet-config/cluster/profiles/manifests/log/server.pp
@@ -26,27 +26,16 @@ class profiles::log::server {
 
   $server_dir    = hiera('profiles::log::server::server_dir')
   $custom_config = hiera('profiles::log::server::custom_config')
-  $firstaction   = hiera('profiles::log::server::logrotate_firstaction', undef)
+  $logrotate_rules = hiera_hash('profiles::log::server::logrotate_rules', {})
 
   # Pass config options as a class parameter
   class { '::rsyslog::server':
     server_dir    => "$server_dir/",
     custom_config => $custom_config,
   }
-  logrotate::rule { 'remotelog':
-    path          => "${server_dir}/*/*",
-    compress      => true,
-    missingok     => true,
-    firstaction   => $firstaction,
-    copytruncate  => true,
-    create        => false,
-    delaycompress => true,
-    mail          => false,
-    rotate        => '30',
-    sharedscripts => true,
-    size          => '5M',
-    rotate_every  => day,
-    postrotate    => 'invoke-rc.d rsyslog rotate > /dev/null',
+
+  class { '::hpc_rsyslog::server':
+    logrotate_rules => $logrotate_rules,
   }
 
   # server_dir may be an NFS-mounted directory. In order to avoid writing
