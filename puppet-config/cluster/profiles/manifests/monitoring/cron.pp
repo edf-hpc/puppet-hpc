6ecb244e47201ee7782a889351b9012970585f46
Philippe Lamare <philippe-externe.lamare@edf.fr>
2017-01-27 16:19:54 +0100

    Ajout fichier sécurisé sur service passif

--- puppet-config/cluster/profiles/manifests/monitoring/cron.pp
+++
@@ -1,9 +0,0 @@
-class profiles::monitoring::cron {
-
-	$cron_file = hiera('profiles::monitoring::cron::cron_file')
-	$cron_source = hiera('profiles::monitoring::cron::cron_source')
-	class { '::icinga2::cron':
-		cron_file    => $cron_file,
-		cron_source => $cron_source,
-	}
-}
--- puppet-config/modules/icinga2/manifests/cron.pp
+++
@@ -1,7 +0,0 @@
-class icinga2::cron ($cron_file, $cron_source) {
-	validate_absolute_path($cron_file)
-	validate_string($cron_source)
-	file { [ $cron_file ] :
-		content => hpc_source_file($cron_source),
-	}
-}
