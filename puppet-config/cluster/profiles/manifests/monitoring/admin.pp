f584ce8413ed25f31f9a001de4835c0e1ac279fb
Philippe Lamare <philippe-externe.lamare@edf.fr>
2017-01-27 16:19:54 +0100

    Ajout fichier sécurisé sur service passif

---
+++ puppet-config/cluster/profiles/manifests/monitoring/admin.pp
@@ -0,0 +1,15 @@
+class profiles::monitoring::admin {
+
+	$cron_file = hiera('profiles::monitoring::admin::cron_file')
+	$cron_source = hiera('profiles::monitoring::admin::cron_source')
+	$pass_file = hiera('profiles::monitoring::admin::pass_file')
+	$pass_source = hiera('profiles::monitoring::admin::pass_source')
+	$decrypt_password = hiera('icinga2::decrypt_passwd')
+	class { '::icinga2::admin':
+		cron_file   => $cron_file,
+		cron_source => $cron_source,
+		pass_file   => $pass_file,
+		pass_source => $pass_source,
+		decrypt_password => $decrypt_password,
+	}
+}
---
+++ puppet-config/modules/icinga2/manifests/admin.pp
@@ -0,0 +1,25 @@
+class icinga2::admin ($cron_file, $cron_source, $pass_file, $pass_source, $decrypt_password) {
+	validate_absolute_path($cron_file)
+	validate_string($cron_source)
+	validate_absolute_path($pass_file)
+	validate_string($pass_source)
+	validate_string($decrypt_password)
+
+	file { [ $cron_file ] :
+		content => hpc_source_file($cron_source),
+	}
+	
+	$dir=dirname("$pass_file")
+	file { [ "$dir" ]:
+		ensure => 'directory',
+		mode   => '0755',
+	}
+
+	file { [ "$pass_file" ]:
+		ensure  => 'present',
+		owner   => 'root',
+		group   => 'root',
+		mode    => '0400',
+		content => decrypt($pass_source, $decrypt_password),
+	}
+}
