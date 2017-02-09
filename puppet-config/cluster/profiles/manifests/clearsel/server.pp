aecfc3aeac26ee9344d35dfacf4a0c796e4ccd16
Thomas Hamel <thomas-externe.hamel@edf.fr>
2017-02-09 17:20:08 +0100

    prof:clearsel Use new package name: hpc-hardware-edf

--- puppet-config/cluster/profiles/manifests/clearsel/server.pp
+++ puppet-config/cluster/profiles/manifests/clearsel/server.pp
@@ -1,9 +1,22 @@
-class profiles::clearsel::server {
-  $packages = [ 'clearsel' ]
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2017 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
 
-  package { $packages :
-    ensure => latest,
-  }
+# Clean BMC Serial Event Logs on the cluster
+class profiles::clearsel::server {
+  $packages = [ 'hpc-hardware-edf' ]
+  ensure_packages($packages, {'ensure' => 'latest'})
 
   include ::clearsel::server
 
