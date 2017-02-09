6b792a0958c6421fe3953fc0df8617bac96edfbe
Thomas Hamel <thomas-externe.hamel@edf.fr>
2017-02-09 17:19:39 +0100

    mod:clearsel Use new package path (/etc/hpc-hardware) for conf

--- puppet-config/modules/clearsel/manifests/server.pp
+++ puppet-config/modules/clearsel/manifests/server.pp
@@ -3,15 +3,14 @@ class clearsel::server {
 	$decrypt_password = hiera('clearsel::decrypt_password')
 	$clearsel_source = hiera('clearsel::clearsel_source')
 
-	file { [ "/etc/clearsel" ]:
+	file { [ "/etc/hpc-hardware" ]:
 		ensure => 'directory'
 	}
-	file { [ "/etc/clearsel/clearsel.conf" ]:
+	file { [ "/etc/hpc-hardware/clearsel.conf" ]:
 		ensure  => 'present',
 		owner   => 'root',
 		group   => 'root',
 		mode    => '0400',
 		content => decrypt($clearsel_source, $decrypt_password),
-		require => File['/etc/clearsel'],
 	}
 }
