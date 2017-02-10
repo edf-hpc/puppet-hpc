3ed2361ee94b418f6d3d40006de1ce01f58ccedf
Rémi Palancher <remi-externe.palancher@edf.fr>
2017-02-10 09:43:01 +0100

    mod:sudo: fix default config_options type
    The init class expects an array, so the default value set in params
    class must be an array as well.

--- puppet-config/modules/sudo/manifests/params.pp
+++ puppet-config/modules/sudo/manifests/params.pp
@@ -19,6 +19,6 @@ class sudo::params {
   $packages                   = ['sudo']
   $packages_ensure            = 'present'
   $config_file                = '/etc/sudoers'
-  $config_options             = ''
+  $config_options             = []
 
 }
