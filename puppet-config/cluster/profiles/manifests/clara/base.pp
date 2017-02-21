eb384e333f452e32c06fcdddd9a7256f955797f2
Rémi Palancher <remi-externe.palancher@edf.fr>
2017-02-21 10:04:50 +0100

    prof:clara: use autolookups for clara SSL cert/key
    And remove resources from the profile, everything is now properly
     encapsulated in the clara module.

--- puppet-config/cluster/profiles/manifests/clara/base.pp
+++ puppet-config/cluster/profiles/manifests/clara/base.pp
@@ -17,38 +17,18 @@
 #
 # ## Hiera
 # * `profiles::clara::repos`
-# * `profiles::clara::ssl_directory_source`
-# * `profiles::clara::certificates_directory`
-# * `profiles::clara::certificate_file`
-# * `profiles::clara::key_file`
+#
+# ## Autolookups
+# * `clara::apt_ssl_cert_source`
+# * `clara::apt_ssl_cert_file`
+# * `clara::apt_ssl_key_source`
+# * `clara::apt_ssl_key_file`
+# * `clara::decrypt_password`
 
 class profiles::clara::base {
 
-  # Hiera lookups
-  $directory_source       = hiera('profiles::clara::ssl_directory_source')
-  $certificates_directory = hiera('profiles::clara::certificates_directory')
-  $certificate_file       = hiera('profiles::clara::certificate_file')
-  $key_file               = hiera('profiles::clara::key_file')
-  $decrypt_passwd         = hiera('profiles::clara::decrypt_passwd')
-
   class { '::clara':
     repos_options => hiera_hash('profiles::clara::repos'),
-  }
-
-  file { $certificates_directory :
-    ensure => 'directory',
-  }
-  file { "${certificates_directory}/${certificate_file}" :
-    ensure  => present,
-    content => decrypt("${directory_source}/${certificate_file}.enc", $decrypt_passwd),
-    require => File[$certificates_directory],
-    mode    => '0600',
-  }
-  file { "${certificates_directory}/${key_file}" :
-    ensure  => present,
-    content => decrypt("${directory_source}/${key_file}.enc", $decrypt_passwd),
-    require => File[$certificates_directory],
-    mode    => '0600',
   }
 
 }
