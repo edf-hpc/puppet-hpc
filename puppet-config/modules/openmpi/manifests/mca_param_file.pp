2699332d2d0f6c201378af831b8bec091335d867
Rémi Palancher <remi-externe.palancher@edf.fr>
2017-02-24 18:59:42 +0100

    mod:openmpi: add missing file for ec262c9

---
+++ puppet-config/modules/openmpi/manifests/mca_param_file.pp
@@ -0,0 +1,28 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2017 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+define openmpi::mca_param_file (
+  $data
+) {
+
+  validate_absolute_path($name)
+  validate_hash($data)
+
+  hpclib::print_config { $name :
+    style => 'ini',
+    data  => $data,
+  }
+
+}
