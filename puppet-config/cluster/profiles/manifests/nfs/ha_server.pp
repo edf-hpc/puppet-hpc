6e1d89bf5070010d1247edf0adbe2e865cafbf79
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2017-03-10 17:02:07 +0000

    prof:nfs Add a ha_server profile
    This profile leverage hpc_nfs::ha_server to create a high-availability
    NFS server.

---
+++ puppet-config/cluster/profiles/manifests/nfs/ha_server.pp
@@ -0,0 +1,19 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2017 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# Configures a NFS HA server
+class profiles::nfs::ha_server {
+  include ::hpc_nfs::ha_server
+}
