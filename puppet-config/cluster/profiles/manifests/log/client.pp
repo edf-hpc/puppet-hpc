c04cf730c7e69916e6eb98aa919ee2aaa8c4aadf
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2017-03-17 10:18:48 +0000

    prof:log Add multi destination to client
    The new parameters permits to send some logs to multiple servers.
    The primary use is for sending commands logs to SOC. Some variable
    names change in hiera to avoid having different names between the
    main destination and the new one. A parameter is removed and
    replaced by autolookup.

--- puppet-config/cluster/profiles/manifests/log/client.pp
+++ puppet-config/cluster/profiles/manifests/log/client.pp
@@ -15,25 +15,39 @@
 
 # Log collection on clients
 #
+# There is a main log forward to a server, typically a machine with the
+# log::server profile. The main forward is controlled with the `protocol`,
+# `host` and `port` hiera variables.
+#
+# Supplementary destination can be configured with the remote_servers
+# hiera array. This is an array of hashes with `protocol`, `host` and
+# `port` values. It can also include a `pattern` value that filters what
+# is sent to this remote server. The syntax uses the rsyslog "traditional"
+# selector syntax (`facility.severity[;facility.severity...]`). Default is
+# `*.*`.
+#
 # ## Hiera
-# * `profiles::log::client::remote_type`
-# * `profiles::log::client::log_local`
-# * `profiles::log::client::server`
+# * `profiles::log::client::protocol`
+# * `profiles::log::client::host`
 # * `profiles::log::client::port`
+# * `profiles::log::client::remote_servers` (`hiera_array`) Array of hashes
 class profiles::log::client {
 
   ## Hiera lookups
-
-  $remote_type = hiera('profiles::log::client::remote_type')
-  $log_local   = hiera('profiles::log::client::log_local')
-  $server      = hiera('profiles::log::client::server')
-  $port        = hiera('profiles::log::client::port')
+  $protocol       = hiera('profiles::log::client::protocol')
+  $host           = hiera('profiles::log::client::host')
+  $port           = hiera('profiles::log::client::port')
+  $remote_servers = hiera_array('profiles::log::client::remote_servers')
 
   # Pass config options as a class parameter
   class { '::rsyslog::client':
-    remote_type => $remote_type,
-    log_local   => $log_local,
-    server      => $server,
-    port        => $port,
+    remote_servers => [
+      {
+        protocol => $protocol,
+        host     => $host,
+        port     => $port,
+      },
+      $remote_servers,
+    ]
   }
 }
