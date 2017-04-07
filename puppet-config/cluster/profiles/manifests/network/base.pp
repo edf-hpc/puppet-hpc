4b9431dcbd12a0a5afa8c698e2026a09654c451c
Rémi Palancher <remi-externe.palancher@edf.fr>
2017-04-07 09:36:31 +0200

    prof:network: update profiles for last mod changes
    The network modules has been made thinner:
    
    - all Infiniband related stuff have been moved to a new infiniband
      module.
    - all Omni-Path related stuff have been moved to a new opa::base public
      class.
    
    Then the network profile is updated to reflect those changes. The
    {ib,opa}_enable activation booleans were initially used internally by
    the network class. These parameters are now used at the profile level
    to control which public class is realized. Therefore they are renamed to
    match profile parameters naming convention.

--- puppet-config/cluster/profiles/manifests/network/base.pp
+++ puppet-config/cluster/profiles/manifests/network/base.pp
@@ -20,15 +20,21 @@
 #                                   topology of the cluster
 # * `profiles::network::gw_connect` Name of the network to get the default
 #                                   gateway from.
+# * `profiles::network::ib_enable`  Boolean to control if Infiniband stack is
+#                                   deployed (default: false)
+# * `profiles::network::opa_enable` Boolean to control if Intel Omni-Path stack
+#                                   is deployed (default: false)
 #
 # ## Relevant Autolookups
-# * `network::mlx4load`        Load `mlx4` Driver for Mellanox
+# * `infiniband::mlx4load`     Load `mlx4` Driver for Mellanox
 #                              ConnectX-3 cards
 # * `network::bonding_options` Interface bonding configuration
 class profiles::network::base {
 
   ## Hiera lookups
   $net_keyword  = hiera('profiles::network::gw_connect')
+  $ib_enable  = hiera('profiles::network::ib_enable', false)
+  $opa_enable = hiera('profiles::network::opa_enable', false)
   $net_topology = hiera_hash('net_topology')
   if ! is_hash($net_topology[$net_keyword]) {
     fail("Undefined WAN network ${net_keyword} in \$net_topology.")
@@ -36,9 +42,29 @@ class profiles::network::base {
   $defaultgw = $net_topology[$net_keyword]['gateway']
   $fqdn      = $mymasternet['fqdn']
 
+  if $ib_enable {
+    class { '::infiniband':
+      stage => 'first',
+    }
+  }
+  if $opa_enable {
+    class { '::opa':
+      stage => 'first',
+    }
+  }
   class { '::network':
     defaultgw => $defaultgw,
     stage     => 'first',
     fqdn      => $fqdn,
+  }
+
+  # Infiniband and OPA classes must be realized before network class because
+  # network may configure IP-over-IB interfaces whose initialization of the
+  # inner device is done by the other two classes.
+  if defined(Class['::infiniband']) {
+    Class['::infiniband'] -> Class['::network']
+  }
+  if defined(Class['::opa']) {
+    Class['::opa'] -> Class['::network']
   }
 }
