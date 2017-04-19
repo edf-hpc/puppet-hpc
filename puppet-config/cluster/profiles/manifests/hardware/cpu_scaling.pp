f969febdcb733041067c762f902c3d7c9f494b18
Benoit Boccard <benoit-externe.boccard@edf.fr>
2017-04-19 11:48:30 +0200

    prof:hardware rename cpufreq in cpu_scalling, possibility to use cpufreq or pstate

---
+++ puppet-config/cluster/profiles/manifests/hardware/cpu_scaling.pp
@@ -0,0 +1,44 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2017 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
+# Activate cpu frequency scaling
+# ## Hiera
+# * `profiles::hardware::cpu_scaling::cpufreq_enable` Boolean to control if
+#                                                     cpufreq method is
+#                                                     used
+# * `profiles::hardware::cpu_scaling::pstate_enable` Boolean to control if
+#                                                    psate method is used
+#
+# ## Relevant Autolookups
+# * `pstate::script_file` Absolute path of the script launched by the service
+# * `pstate::systemd_service_file` Absolute path of the systemd service unit
+#                                  file`
+# * `pstate::systemd_service_file_options Hash with the content of the systemd
+#                                         service unit file`
+# * `cpufreq::packages` Package to install
+# * `cpufreq::packages_ensure` Boolean to control if module manage packages
+# * `cpufreq::default_file` Absolute path of the default configuration file
+# * `cpufreq::default_options` Hash with the content of the default 
+#                              configuration file
+
+class profiles::hardware::cpu_scaling {
+  if $cpufreq_enable {
+    include ::cpufreq
+  }
+
+  if $pstate_enable {
+    include ::pstate
+  }
+}
