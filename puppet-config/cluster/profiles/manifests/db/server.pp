e582fcebb592d6784ecf131135e6d2c1765d249e
Rémi Palancher <remi-externe.palancher@edf.fr>
2017-04-28 08:54:42 +0200

    prof:db: update comments with new mariadb params

--- puppet-config/cluster/profiles/manifests/db/server.pp
+++ puppet-config/cluster/profiles/manifests/db/server.pp
@@ -29,11 +29,11 @@
 #    query_cache_size:         '0'
 #    query_cache_type:         '0'
 #    bind-address:             '0.0.0.0'
+#    wsrep_on:                 'ON'
 #    wsrep_provider:           '/usr/lib/galera/libgalera_smm.so'
 #    wsrep_cluster_name:       '"galera_cluster"'
 #    wsrep_cluster_address:    "\"gcomm://%{hiera('galera_base_name')}1,%{hiera('galera_base_name')}2\""
 #    wsrep_sst_method:         'rsync'
-# mariadb::mysql_root_pwd:      'GALERA_ROOT_PASSWORD_OVERRIDEME_IN_EYAML'
 # ```
 class profiles::db::server {
   include ::mariadb
