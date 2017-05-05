f47b73f6c45530dec1719e106dab041d0819fe55
GILBERT Nicolas <nicolas-externe.gilbert@edf.fr>
2017-05-05 11:44:58 +0200

    mod: icinga2: add config file for check_xxx

--- puppet-config/cluster/profiles/manifests/monitoring/admin.pp
+++ puppet-config/cluster/profiles/manifests/monitoring/admin.pp
@@ -2,12 +2,16 @@ class profiles::monitoring::admin {
 
   $cron_file = hiera('profiles::monitoring::admin::cron_file')
   $cron_source = hiera('profiles::monitoring::admin::cron_source')
+  $conf_check_file = hiera('profiles::monitoring::admin::conf_check_file')
+  $conf_check_source = hiera('profiles::monitoring::admin::conf_check_source')
   $pass_file = hiera('profiles::monitoring::admin::pass_file')
   $pass_source = hiera('profiles::monitoring::admin::pass_source')
   $decrypt_password = hiera('icinga2::decrypt_passwd')
   class { '::icinga2::admin':
     cron_file   => $cron_file,
     cron_source => $cron_source,
+    conf_check_file => $conf_check_file,
+    conf_check_source => $conf_check_source,
     pass_file   => $pass_file,
     pass_source => $pass_source,
     decrypt_password => $decrypt_password,
--- puppet-config/modules/icinga2/manifests/admin.pp
+++ puppet-config/modules/icinga2/manifests/admin.pp
@@ -1,6 +1,8 @@
-class icinga2::admin ($cron_file, $cron_source, $pass_file, $pass_source, $decrypt_password) {
+class icinga2::admin ($cron_file, $cron_source, $pass_file, $pass_source, $decrypt_password, $conf_check_file, $conf_check_source) {
 	validate_absolute_path($cron_file)
 	validate_string($cron_source)
+	validate_absolute_path($conf_check_file)
+	validate_string($conf_check_source)
 	validate_absolute_path($pass_file)
 	validate_string($pass_source)
 	validate_string($decrypt_password)
@@ -8,6 +10,10 @@ class icinga2::admin ($cron_file, $cron_source, $pass_file, $pass_source, $decry
 	file { [ $cron_file ] :
 		content => hpc_source_file($cron_source),
 	}
+
+	file { [ $conf_check_file ] :
+                content => hpc_source_file($conf_check_source),
+        }
 	
 	$dir=dirname("$pass_file")
 	file { [ "$dir" ]:
