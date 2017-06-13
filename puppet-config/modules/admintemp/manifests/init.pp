34a99fc88affbb93690a4e0b3fcd1d9da4e6f7eb
GILBERT Nicolas <nicolas-externe.gilbert@edf.fr>
2017-06-13 08:33:54 +0200

    mod:admintemp: replace tabs with spaces for indent

--- puppet-config/modules/admintemp/manifests/init.pp
+++ puppet-config/modules/admintemp/manifests/init.pp
@@ -1,25 +1,25 @@
 class admintemp {
-	file { [ '/usr/local/sbin/manage-admin-temporaire' ]:
-		source => 'puppet:///modules/admintemp/manage-admin-temporaire',
-		ensure => file,
-		owner => root,
-		group => root,
-		mode => 700,
-	}
-	$admin_temporaire_cron = hiera('admintemp::manage-admin-temporaire_cron')
+        file { [ '/usr/local/sbin/manage-admin-temporaire' ]:
+                source => 'puppet:///modules/admintemp/manage-admin-temporaire',
+                ensure => file,
+                owner => root,
+                group => root,
+                mode => 700,
+        }
+        $admin_temporaire_cron = hiera('admintemp::manage-admin-temporaire_cron')
 
-	file { [ '/etc/cron.d/manage-admin-temporaire' ]:
-		content => hpc_source_file($admin_temporaire_cron),
-		ensure => file,
-		owner => root,
-		group => root,
-		mode => 750,
-	}
-	group { [ 'admin-temporaire' ]:
-		ensure => present,
-		forcelocal => 'true',
-	}
-	$admin_temporaire_conf=hiera('admintemp::manage-admin-temporaire_conf')
+        file { [ '/etc/cron.d/manage-admin-temporaire' ]:
+                content => hpc_source_file($admin_temporaire_cron),
+                ensure => file,
+                owner => root,
+                group => root,
+                mode => 750,
+        }
+        group { [ 'admin-temporaire' ]:
+                ensure => present,
+                forcelocal => 'true',
+        }
+        $admin_temporaire_conf=hiera('admintemp::manage-admin-temporaire_conf')
         file { [ '/etc/manage-admin-temporaire.conf' ] :
                 content => hpc_source_file($admin_temporaire_conf),
         }
