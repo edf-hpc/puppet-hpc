3c2e99da4acc3baf72335faacfefc64d62e51d2a
Thomas Hamel <thomas-externe.hamel@edf.fr>
2017-07-17 17:18:01 +0200

    mod:cpufreq Variable name fix and other lint fixes

--- puppet-config/modules/cpufreq/manifests/init.pp
+++ puppet-config/modules/cpufreq/manifests/init.pp
@@ -18,19 +18,19 @@
 # @param packages Package list
 # @param packages_ensure Package install mode (default:
 #                        'present)
-# @param default_file Default configuration file path (default: 
+# @param default_file Default configuration file path (default:
 #                     '/etc/default/cpufrequtils')
-# @param default_options Hash with the content of `default_file` 
+# @param default_options Hash with the content of `default_file`
 #                        (merged with defaults)
 # @param service_ensure   Should the service run or be stopped (default: running)
 # @param service_enable Boolean: is the service starting at boot (default: true)
 
 class cpufreq (
-  $packages        = $cpufreq::params::packages,
-  $packages_ensure = $cpufreq::params::packages_ensure,
-  $default_file    = $cpufreq::params::default_file,
-  $service_ensure  = $cpufreq::params::service_ensure,
-  $service_enable  = $cpufreq::params::service_enable,  
+  $packages        = $::cpufreq::params::packages,
+  $packages_ensure = $::cpufreq::params::packages_ensure,
+  $default_file    = $::cpufreq::params::default_file,
+  $service_ensure  = $::cpufreq::params::service_ensure,
+  $service_enable  = $::cpufreq::params::service_enable,
   $default_options = {},
 ) inherits cpufreq::params {
 
@@ -39,7 +39,7 @@ class cpufreq (
   validate_absolute_path($default_file)
   validate_hash($default_options)
 
-  $_default_options=deep_merge($cpufreq::params::default_options_defaults, $defaults_options)
+  $_default_options=deep_merge($::cpufreq::params::default_options_defaults, $default_options)
 
   anchor { 'cpufreq::begin': } ->
   class { '::cpufreq::install': } ->
