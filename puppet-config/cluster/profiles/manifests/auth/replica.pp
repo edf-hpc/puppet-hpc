0e7072d6aa1765bf971133d4f7c4357e90b4a100
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2017-08-22 17:23:08 +0200

    prof:auth Support listen_network for replica
    A new hiera key specifies a list of network on which slapd will listen
    if it is available on the node. By default the list is empty and
    slapd listens on all networks.

--- puppet-config/cluster/profiles/manifests/auth/replica.pp
+++ puppet-config/cluster/profiles/manifests/auth/replica.pp
@@ -1,3 +1,18 @@
+##########################################################################
+#  Puppet configuration file                                             #
+#                                                                        #
+#  Copyright (C) 2014-2017 EDF S.A.                                      #
+#  Contact: CCN-HPC <dsp-cspit-ccn-hpc@edf.fr>                           #
+#                                                                        #
+#  This program is free software; you can redistribute in and/or         #
+#  modify it under the terms of the GNU General Public License,          #
+#  version 2, as published by the Free Software Foundation.              #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+##########################################################################
+
 class profiles::auth::replica {
 
   ## Hiera lookups
@@ -5,11 +20,27 @@ class profiles::auth::replica {
   $directory_source = hiera('profiles::auth::replica::directory_source')
   $ldif_file        = hiera('profiles::auth::replica::ldif_file')
   $ldif_directory   = hiera('profiles::auth::replica::ldif_directory')
+  $listen_networks  = hiera_array('profiles::auth::replica::listen_networks', [])
+
+  # If listening interfaces are provided add it to the list of listening
+  # URIS in /etc/default/slapd
+  if size($listen_networks) > 0 {
+    $ip_addrs = hpc_net_ip_addrs($listen_networks)
+    $uris = prefix($ip_addrs, 'ldaps://')
+    $joined_uris = join($uris, ' ')
+    $default_options = {
+      'SLAPD_SERVICES' => "'ldapi:// ${joined_uris}'",
+    }
+  } else {
+    $default_options = undef
+  }
 
   # Pass config options as a class parameter
   include certificates
 
-  include ::openldap
+  class { '::openldap' :
+    default_options => $default_options,
+  }
 
   class { '::openldap::replica' :
     directory_source => $directory_source,
