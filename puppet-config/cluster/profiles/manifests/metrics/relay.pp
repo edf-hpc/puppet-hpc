eb13b73de58da9421d66a84cceb1bce268ae7663
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2017-09-06 18:23:04 +0200

    prof:metrics relay: Support for listen_address hiera key
    Add a hiera key to specify a specific IP address the carbon-c-relay
    server should listen on.
    
    Since this address might not be available during system start,
    this patch also sets the sysctl net.ipv4.ip_nonlocal_bind to
    authorize binding an interface that does not exists.

--- puppet-config/cluster/profiles/manifests/metrics/relay.pp
+++ puppet-config/cluster/profiles/manifests/metrics/relay.pp
@@ -19,8 +19,29 @@
 # * `profiles::metrics::relay_destination_clusters` (`hiera_array`)
 # * `profiles::metrics::relay_match_rules` (`hiera_array`)
 # * `profiles::metrics::relay_rewrite_rules` (`hiera_array`)
+# * `profiles::metrics::relay::listen_address` Address the carbon-c-relay
+#     server will listen on (default: '0.0.0.0')
 class profiles::metrics::relay {
-  include ::carboncrelay
+
+  $listen_address = hiera('profiles::metrics::relay::listen_address', '0.0.0.0')
+  if $listen_address == '0.0.0.0' {
+    $listen_address_actual = undef
+  } else {
+    $listen_address_actual = $listen_address
+
+    ## Sysctl setup
+    # We need a sysctl to enable the ip_nonlocal_bind that will permit
+    # carbon-c-relay to bind the VIP on the failover node
+    kernel::sysctl { 'profiles_metrics_relay':
+      params => {
+        'net.ipv4.ip_nonlocal_bind' => '1',
+      },
+    }
+  }
+
+  class { '::carboncrelay' :
+    listen_address => $listen_address_actual,
+  }
 
   $clusters = hiera_hash('profiles::metrics::relay_destination_clusters')
   create_resources(::carboncrelay::cluster, $clusters)
