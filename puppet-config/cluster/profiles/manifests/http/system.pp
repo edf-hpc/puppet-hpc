a9c7c71386de315ffed0629935412d269f11dccc
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2017-09-06 14:56:16 +0200

    prof:http system: Also listen on VIP from listen_networks
    If the client use the VIP and not the consul DNS interface this
    patch will make that work.
    
    This patch add all VIPs of the node to the listen interfaces, since
    some of those addresses might not be available during system start,
    this patch also sets the sysctl net.ipv4.ip_nonlocal_bind to
    authorize binding an interface that does not exists.

--- puppet-config/cluster/profiles/manifests/http/system.pp
+++ puppet-config/cluster/profiles/manifests/http/system.pp
@@ -44,13 +44,25 @@ class profiles::http::system {
   $servername = "${cluster_prefix}${::puppet_role}"
   $serveraliases = ["${servername}.${domain}"]
 
-  # If listening interfaces are provided add it to the list of listening
-  # addresses in the config
   if size($listen_networks) > 0 {
-    $ip_addrs = hpc_net_ip_addrs($listen_networks)
+    # If listening interfaces are provided add it to the list of listening
+    # addresses in the config (including VIPs)
+    $ip_addrs = hpc_net_ip_addrs($listen_networks, true)
     $ip = ['127.0.0.1', $ip_addrs]
+
+
+    ## Sysctl setup
+    # We need a sysctl to enable the ip_nonlocal_bind that will permit
+    # apache to bind the VIP on de failover node
+    kernel::sysctl { 'profiles_http_system':
+      params => {
+        'net.ipv4.ip_nonlocal_bind' => '1',
+      },
+    }
+
   } else {
     $ip = undef
+
   }
 
   # Pass config options as a class parameter
