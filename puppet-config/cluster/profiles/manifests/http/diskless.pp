2e5d2b08b6c2239cef22079372a92cce78154d07
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2017-09-06 14:51:22 +0200

    prof:http diskless: Add support for listen_networks parameter
    The listen_networks hiera key permits to set a lis of networks that
    will be used by the vhost to listen to.
    
    This patch add all VIPs of the node to the listen interfaces. Since
    some of those addresses might not be available during system start,
    this patch also sets the sysctl net.ipv4.ip_nonlocal_bind to
    authorize binding an interface that does not exists.

--- puppet-config/cluster/profiles/manifests/http/diskless.pp
+++ puppet-config/cluster/profiles/manifests/http/diskless.pp
@@ -23,24 +23,48 @@
 # * `profiles::http::diskless::port`
 # * `profiles::http::diskless::docroot`
 # * `profiles::http::serveradmin`
+# * `profiles::http::diskless::listen_networks` (`hiera_array`) List of network
+#     the diskless vhost daemon should bind, all if ommited or empty
 class profiles::http::diskless {
 
   ## Hiera lookups
-  $serveradmin    = hiera('profiles::http::serveradmin')
-  $log_level      = hiera('profiles::http::log_level')
-  $port           = hiera('profiles::http::diskless::port')
-  $docroot        = hiera('profiles::http::diskless::docroot')
-  $cluster_prefix = hiera('cluster_prefix')
-  $domain         = hiera('domain')
+  $serveradmin     = hiera('profiles::http::serveradmin')
+  $log_level       = hiera('profiles::http::log_level')
+  $port            = hiera('profiles::http::diskless::port')
+  $docroot         = hiera('profiles::http::diskless::docroot')
+  $listen_networks = hiera_array('profiles::http::diskless::listen_networks', [])
+  $cluster_prefix  = hiera('cluster_prefix')
+  $domain          = hiera('domain')
 
   include apache
 
   $servername = "${cluster_prefix}${::puppet_role}"
   $serveraliases = ["${servername}.${domain}"]
 
+  if size($listen_networks) > 0 {
+    # If listening interfaces are provided add it to the list of listening
+    # addresses in the config (including VIPs)
+    $ip_addrs = hpc_net_ip_addrs($listen_networks, true)
+    $ip = ['127.0.0.1', $ip_addrs]
+
+    ## Sysctl setup
+    # We need a sysctl to enable the ip_nonlocal_bind that will permit
+    # apache to bind the VIP on de failover node
+    kernel::sysctl { 'profiles_http_diskless':
+      params => {
+        'net.ipv4.ip_nonlocal_bind' => '1',
+      },
+    }
+
+  } else {
+    $ip = undef
+
+  }
+
   # Pass config options as a class parameter
   apache::vhost { "${servername}_diskless":
     servername    => $servername,
+    ip            => $ip,
     port          => $port,
     docroot       => $docroot,
     serveradmin   => $serveradmin,
