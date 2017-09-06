75aac6f76e0714eef5c36cbaabd74d0a343dc49c
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2017-09-06 14:57:45 +0200

    prof:inventory collect: Add listen_networks parameter for vhost
    For security purpose it is preferable to restrict the listen interface
    of the vhost to the ones that will actually be used.
    
    This patch add all VIPs of the node to the listen interfaces, since
    some of those addresses might not be available during system start,
    this patch also sets the sysctl net.ipv4.ip_nonlocal_bind to
    authorize binding an interface that does not exists.

--- puppet-config/cluster/profiles/manifests/inventory/collect.pp
+++ puppet-config/cluster/profiles/manifests/inventory/collect.pp
@@ -20,15 +20,38 @@
 # * `domain`
 #
 # ## Relevant Autolookups
+# * `profiles::inventory::collect::listen_networks` (`hiera_array`) List of network
+#     the diskless vhost daemon should bind, all if ommited or empty
 #
 class profiles::inventory::collect {
 
   $prefix          = hiera('cluster_prefix')
   $domain          = hiera('domain')
   $virtual_address = $::hostfile["${prefix}${::puppet_role}"]
+  $listen_networks = hiera_array('profiles::inventory::collect::listen_networks', [])
   $servername      = "${prefix}${::puppet_role}"
   $serveraliases   = ["${servername}.${domain}"]
 
+  if size($listen_networks) > 0 {
+    # If listening interfaces are provided add it to the list of listening
+    # addresses in the config (including VIPs)
+    $ip_addrs = hpc_net_ip_addrs($listen_networks, true)
+    $ip = ['127.0.0.1', $ip_addrs]
+
+
+    ## Sysctl setup
+    # We need a sysctl to enable the ip_nonlocal_bind that will permit
+    # apache to bind the VIP on de failover node
+    kernel::sysctl { 'profiles_inventory_collect':
+      params => {
+        'net.ipv4.ip_nonlocal_bind' => '1',
+      },
+    }
+
+  } else {
+    $ip = undef
+
+  }
 
   include apache
   include apache::mod::php
@@ -37,6 +60,7 @@ class profiles::inventory::collect {
     virtual_address => $virtual_address,
     servername      => $servername,
     serveraliases   => $serveraliases,
+    ip              => $ip,
   }
 
 }
