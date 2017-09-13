20156dc5f5bb1f3b4a3625ac825b048d3492bcf9
Thomas HAMEL <thomas-externe.hamel@edf.fr>
2017-09-13 15:21:38 +0200

    prof:p2p implement listen_networks for tracker
    Permits to define which network the tracker will listen on with
    the hiera array profiles::p2p::tracker::listen_networks. If
    empty, use the default from the opentracker module (all interfaces)

--- puppet-config/cluster/profiles/manifests/p2p/tracker.pp
+++ puppet-config/cluster/profiles/manifests/p2p/tracker.pp
@@ -29,14 +29,30 @@
 # ## Facts
 # * `hosts_by_role['admin'][0]`
 # * `hosts_by_role["$my_p2p_tracker"]`
+#
+# ## Hiera
+# * `profiles::p2p::tracker::listen_networks` (`hiera_array`) List of network
+#     the diskless vhost daemon should bind, all if ommited or empty
 
 class profiles::p2p::tracker {
-
+  $listen_networks = hiera_array('profiles::p2p::tracker::listen_networks')
   $admin_node    = $hosts_by_role['admin'][0]
   $tracker_nodes = $hosts_by_role["$my_p2p_tracker"]
 
+  if size($listen_networks) > 0 {
+    # If listening interfaces are provided add it to the list of listening
+    # addresses in the config (including VIPs)
+    $ip = hpc_net_ip_addrs($listen_networks, true)
+
+  } else {
+    $ip = undef
+
+  }
+
+
   class { '::opentracker':
-    admin_node    => $admin_node,
-    tracker_nodes => $tracker_nodes,
+    admin_node          => $admin_node,
+    tracker_nodes       => $tracker_nodes,
+    listen_ip_addresses => $ip,
   }
 }
