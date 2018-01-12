8964cf0da00536411d28fc375a9adb4967416c83
Thomas Hamel <thomas-externe.hamel@edf.fr>
2018-01-12 17:08:51 +0100

    mod:clearsel Automatic lint whitespace fix

--- puppet-config/modules/clearsel/manifests/server.pp
+++ puppet-config/modules/clearsel/manifests/server.pp
@@ -1,16 +1,16 @@
 class clearsel::server {
 
-	$decrypt_password = hiera('clearsel::decrypt_password')
-	$clearsel_source = hiera('clearsel::clearsel_source')
+  $decrypt_password = hiera('clearsel::decrypt_password')
+  $clearsel_source = hiera('clearsel::clearsel_source')
 
-	file { [ "/etc/hpc-hardware" ]:
-		ensure => 'directory'
-	}
-	file { [ "/etc/hpc-hardware/clearsel.conf" ]:
-		ensure  => 'present',
-		owner   => 'root',
-		group   => 'root',
-		mode    => '0400',
-		content => decrypt($clearsel_source, $decrypt_password),
-	}
+  file { [ "/etc/hpc-hardware" ]:
+    ensure => 'directory'
+  }
+  file { [ "/etc/hpc-hardware/clearsel.conf" ]:
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    content => decrypt($clearsel_source, $decrypt_password),
+  }
 }
